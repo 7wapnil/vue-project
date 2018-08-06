b49d5d96bd7b67209f76b5b2cd52589d41650718
Rahul <rmishra@gammastack.com>
2018-08-06 11:18:00 +0300

    Add stack input field to market in beslip

--- src/components/betslip/MarketInBetslip.vue
+++ src/components/betslip/MarketInBetslip.vue
@@ -1,32 +1,33 @@
 <template lang="pug">
-    .div
-        .div.my-4
+    div
+        h5
+            ="USION.BET MASTERS: One vs Two"
+        .my-4
             = "Match Winner"
             button.close(aria-label="Close")
                 span(aria-hidden="true")
-                    ="×"   
+                    ="×"
         .row
-            .m-2.col 
+            .m-2.col
                 odd-button(:odd={name:"winner 1",value:"2.2"})
-            .m-2.col 
+            .m-2.col
                 odd-button(:odd={name:"draw",value:"1"})
-            .m-2.col 
-                odd-button(:odd={name:"winner w",value:"3.23"})
-        .div.mt-4.text-right
-            .row.my-2
-                .col.text-nowrap.text-right
-                    = "My Stake:"
-                    br
-                    button.btn.btn-sm.btn-outline-secondary.my-2             
-                        = "Place max bet"
-                .col-2
-                    = "0"
-            .row.my-2.text-nowrap
-                .col.text-nowrap.text-right
-                    = "Potential Return:"
-                .col-2
-                    = "NaN"  
-        hr                  
+        .row.mt-4.text-right
+            .col-12
+                .row.my-2
+                    .col-8.text-nowrap.text-right
+                        = "My Stake:"
+                        br
+                        button.btn.btn-sm.btn-outline-secondary.my-2
+                            = "Place max bet"
+                    .col-4
+                        input.form-control(type='text', name='odd-value', value=0)
+                .row.my-2
+                    .col-8.text-nowrap.text-right
+                        = "Potential Return:"
+                    .col-4
+                        = "NaN"
+        hr
 </template>
 
 <script>
