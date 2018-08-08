a93356fb0d9b7557a576f9e303c570b21f9aebfa
Rahul <rmishra@gammastack.com>
2018-08-08 10:40:59 +0300

    Unpug betslip template

--- src/components/betslip/Betslip.vue
+++ src/components/betslip/Betslip.vue
@@ -1,37 +1,39 @@
-<template lang="pug">
-    b-card#betslip(header="Betslip(2)")
-        b-tabs.mt-2
-            b-tab(title='Single', active='').p-2.px-2
-                div#markets-in-betslip.mt-4
-                    div(v-for="odd in betslipOdds" :key="odd.id")
-                         market-in-betslip
-                div.mt-4.text-right
-                        .row.my-2#total-odds
-                            .col.text-nowrap.text-right
-                                = "Total Odds:"
-                            .col-2#total-odds-value
-                                = "0"
-                        .row.my-2#total-stake
-                            .col.text-nowrap.text-right
-                                = "Total stake:"
-                            .col-2#total-stake-value
-                                = "0"
-                        .row.my-2#total-return
-                            .col.text-nowrap.text-right
-                                = "Total return:"
-                            .col-2#total-return-value
-                                = "0"
-                        .row_
-                div.mt-4
-                    button#placeBets.btn.btn-danger.btn-lg.btn-block(v-on:click="$emit('placeBetslip')")
-                        = "Place bet"
-            b-tab(title='Accumulator')
-                p.m-3
-                    = "To be implemented"
-            b-tab(title='System')
-                p.m-3
-                    = "To be implemented"
-
+<template>
+    <a>
+        <b-card id="betslip" header="Betslip(2)">
+            <b-tabs class="mt-2">
+                <b-tab class="p-2 px-2" title="Single" active="">
+                    <div class="mt-4" id="markets-in-betslip">
+                        <div v-for="odd in betslipOdds" :key="odd.id">
+                            <market-in-betslip/>
+                        </div>
+                    </div>
+                    <div class="mt-4 text-right">
+                        <div class="row my-2" id="total-odds">
+                            <div class="col text-nowrap text-right">Total Odds:</div>
+                            <div class="col-2" id="total-odds-value">0</div>
+                        </div>
+                        <div class="row my-2" id="total-stake">
+                            <div class="col text-nowrap text-right">Total stake:</div>
+                            <div class="col-2" id="total-stake-value">0</div>
+                        </div>
+                        <div class="row my-2" id="total-return">
+                            <div class="col text-nowrap text-right">Total return:</div>
+                            <div class="col-2" id="total-return-value">0</div>
+                        </div>
+                        <div class="row"></div>
+                    </div>
+                    <div class="mt-4">
+                        <button class="btn btn-danger btn-lg btn-block" id="placeBets"
+                                v-on:click="$emit('placeBetslip')">Place bet
+                        </button>
+                    </div>
+                </b-tab>
+                <b-tab title="Accumulator"><p class="m-3">To be implemented</p></b-tab>
+                <b-tab title="System"><p class="m-3">To be implemented</p></b-tab>
+            </b-tabs>
+        </b-card>
+    </a>
 </template>
 
 <script>
