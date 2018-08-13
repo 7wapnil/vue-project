ce66bce1a167f0fd10e74aa4739dbce0e7dbad84
Rahul <rmishra@gammastack.com>
2018-08-13 11:25:09 +0300

    Export wallets functionality to Vuex store

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './user'
 import betslip from './betslip'
+import wallets from './wallets'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     user,
-    betslip  
+    betslip,
+    wallets
   }
 })
--- tests/unit/specs/components/BalancesList.spec.js
+++ tests/unit/specs/components/BalancesList.spec.js
@@ -32,66 +32,57 @@ localVue.use(Vuex)
 describe('BalancesList component', () => {
 
   let wrapper
-  let store
-  let walletId
-
-  let setActiveWallet = (id) => {
-    walletId = id
-    wrapper.vm.setActiveWallet()
-  }
+  let getters
+  let mutations
+  let actions
 
   beforeEach(() => {
-    walletId = null
+    const state = {
+      wallets: wallets
+    }
 
-    store = new Vuex.Store({
-      state: {},
-      mutations: {},
-      getters: {
-        getUserData: () => () => {
-          return walletId
-        }
-      }
+    getters = {
+      getActiveWallet: () => wallets[1],
+      getWallets: () => wallets
+    }
+
+    mutations = {
+      setActiveWalletId: sinon.stub()
+    }
+
+    actions = {
+      loadWallets: sinon.stub()
+    }
+
+    const store = new Vuex.Store({
+      state,
+      getters,
+      mutations,
+      actions
     })
 
     wrapper = shallowMount(BalancesList, {
       localVue,
-      store,
-      methods: {
-        loadWallets: () => {}
-      }
+      store
     })
-    wrapper.setData({ wallets })
   })
 
   describe('Setting active wallet', () => {
-    it('should hide dropdown unless active wallet set', () => {
-      expect(wrapper.vm.activeWallet).to.be.null
-    })
-
     it('should load active wallet ID from store', () => {
-      setActiveWallet(2)
-      wrapper.vm.setActiveWallet()
-      expect(wrapper.find('#wallets-dropdown').text()).to.eq('19.99 BTC')
-    })
-
-    it('should set first wallet active if no wallet stored', () => {
-      setActiveWallet(null)
-      expect(wrapper.find('#wallets-dropdown').text()).to.eq('299.00 EUR')
+      const text = wrapper.vm.displayAmount(wallets[1])
+      expect(wrapper.find('#wallets-dropdown').text()).to.eq(text)
     })
   })
 
   describe('List of customer wallets', () => {
     it ('should exclude active wallet from a list', () => {
-      setActiveWallet(1)
       expect(wrapper.findAll('a.dropdown-item').length).to.eq(2)
     })
 
     it ('should set other active wallet on select', () => {
-      setActiveWallet(1)
-
-      expect(wrapper.vm.activeWallet.id).to.eq(1)
       wrapper.find('a.dropdown-item').trigger('click')
-      expect(wrapper.vm.activeWallet.id).to.eq(2)
+      expect(mutations.setActiveWalletId.calledOnce).to.equal(true)
+      expect(mutations.setActiveWalletId.firstCall.args[1]).to.eq(1);
     })
   })
 
