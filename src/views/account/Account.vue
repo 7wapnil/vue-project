6a1a863e05e30ce5d55ef3149ded9574a8a8431f
Mahendra <mtyagi@gammastack.com>
2018-10-08 14:51:37 +0300

    Change user profile fields layout

--- src/views/account/Account.vue
+++ src/views/account/Account.vue
@@ -10,37 +10,66 @@
     <b-row>
       <b-col>
         <b-form>
-          <b-form-group label="Email:"/>
-          <b-form-group label="Username:"/>
-          <b-form-group label="Gender:"/>
-          <b-form-group label="Birthday:"/>
-          <b-form-group label="Phone:"/>
+          <b-form-group
+            :label-cols="2"
+            horizontal
+            label-size="sm"
+            label-for="email"
+            label="Email:">
+            <p>{{ user.email }}</p>
+          </b-form-group>
+          <b-form-group
+            :label-cols="2"
+            horizontal
+            label-size="sm"
+            label-for="username"
+            label="Username:">
+            <b-form-input
+              id="username"
+              :value="user.username"
+              size="sm"
+              disabled/>
+          </b-form-group>
+          <b-form-group
+            :label-cols="2"
+            horizontal
+            label-size="sm"
+            label-for="gender"
+            label="Gender:">
+            <b-form-select
+              id="gender"
+              :value="user.gender"
+              disabled>
+              <option selected>
+                {{ user.gender }}
+              </option>
+            </b-form-select>
+          </b-form-group>
+          <b-form-group
+            :label-cols="2"
+            horizontal
+            label-size="sm"
+            label-for="birthday"
+            label="Birthday:">
+            <b-form-input
+              id="birthday"
+              :value="user.date_of_birth"
+              disabled
+              type="date"/>
+          </b-form-group>
+          <b-form-group
+            :label-cols="2"
+            horizontal
+            label-size="sm"
+            label-for="phone"
+            label="Phone:">
+            <b-form-input
+              id="phone"
+              :value="user.phone"
+              disabled
+              type="tel"/>
+          </b-form-group>
         </b-form>
-      </b-col>
-      <b-col class="mr-5">
-        <p>{{ user.email }}</p>
-        <b-form-input
-          :value="user.username"
-          disabled
-          class="mb-2"/>
-        <b-form-select
-          :value="user.gender"
-          disabled
-          class="mb-2">
-          <option selected>
-            {{ user.gender }}
-          </option>
-        </b-form-select>
-        <b-form-input
-          :value="user.date_of_birth"
-          disabled
-          class="mb-2"
-          type="date"/>
-        <b-form-input
-          :value="user.phone"
-          disabled
-          class="mb-2"
-          type="tel"/>
       </b-col>
     </b-row>
   </b-container>
