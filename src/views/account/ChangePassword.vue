c971213552c95fee92c5f2d2daf50792cce82fa9
Mahendra <mtyagi@gammastack.com>
2018-10-10 16:31:57 +0300

    Add change password form

--- src/views/account/ChangePassword.vue
+++ src/views/account/ChangePassword.vue
@@ -2,6 +2,11 @@
   <b-container>
     <b-row>
       <b-col>
+        <h1 class="mb-4">Password</h1>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
         <b-form-group
           :label-cols="4"
           horizontal
@@ -11,6 +16,9 @@
           label-for="current_password">
           <b-form-input
             id="current_password"
+            v-model="existing_password"
+            type="password"
+            autocomplete="current-password"
             size="md"/>
         </b-form-group>
         <b-form-group
@@ -22,6 +30,8 @@
           label-for="new_password">
           <b-form-input
             id="new_password"
+            v-model="new_password"
+            type="password"
             size="md"/>
         </b-form-group>
         <b-form-group
@@ -33,14 +43,18 @@
           label-for="repeat_password">
           <b-form-input
             id="repeat_password"
+            v-model="new_password_confirmation"
+            type="password"
             size="md"/>
         </b-form-group>
       </b-col>
     </b-row>
     <b-row>
-      <b-col class="text-right">
-        <b-button variant="primary">
-          Reset password
+      <b-col class="text-right mt-3">
+        <b-button
+          variant="primary"
+          @click="changePassword">
+          Change password
         </b-button>
       </b-col>
     </b-row>
@@ -48,9 +62,32 @@
 </template>
 
 <script>
+import { CHANGE_PASSWORD } from '@/graphql'
+
 export default {
   data () {
-    return {}
+    return {
+      existing_password: '',
+      new_password: '',
+      new_password_confirmation: ''
+    }
+  },
+  methods: {
+    changePassword () {
+      this.$apollo.mutate({
+        mutation: CHANGE_PASSWORD,
+        variables: {
+          existing_password: this.existing_password,
+          new_password: this.new_password,
+          new_password_confirmation: this.new_password_confirmation
+        }
+      }).then(() => {
+        this.$noty.success('Your password successfully changed')
+      }).catch((err) => {
+        console.log('Error: ', err)
+        this.$noty.error(err.message)
+      })
+    }
   }
 }
 </script>
