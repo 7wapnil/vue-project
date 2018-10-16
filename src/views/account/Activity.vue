4b3fc0a684ed1ac5e6d0730ab65ecf497273f7f2
Mahendra <mtyagi@gammastack.com>
2018-10-16 15:58:58 +0300

    Create activities page layout

---
+++ src/views/account/Activity.vue
@@ -0,0 +1,23 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col>
+                <h1 class="mb-4 ml-4">
+                    My activity
+                </h1>
+            </b-col>
+        </b-row>
+        <b-tabs card>
+            <b-tab title="Esport" active>
+                Esport activities
+            </b-tab>
+            <b-tab title="Sport">
+                Sport activities
+            </b-tab>
+            <b-tab title="Casino">
+                Casino activities
+            </b-tab>
+        </b-tabs>
+    </b-container>
+</template>
+
