db7a200764fc51771593912b91a7313896e5ecd4
Mahendra <mtyagi@gammastack.com>
2018-10-16 16:13:13 +0300

    Create activities tabs

--- src/views/account/Activity.vue
+++ src/views/account/Activity.vue
@@ -1,23 +1,38 @@
 <template>
-    <b-container>
-        <b-row>
-            <b-col>
-                <h1 class="mb-4 ml-4">
-                    My activity
-                </h1>
-            </b-col>
-        </b-row>
-        <b-tabs card>
-            <b-tab title="Esport" active>
-                Esport activities
-            </b-tab>
-            <b-tab title="Sport">
-                Sport activities
-            </b-tab>
-            <b-tab title="Casino">
-                Casino activities
-            </b-tab>
-        </b-tabs>
-    </b-container>
+  <b-container>
+    <b-row>
+      <b-col>
+        <h1 class="mb-4 ml-4">
+          My activity
+        </h1>
+      </b-col>
+    </b-row>
+    <b-tabs card>
+      <b-tab
+        v-for="tab in tabs"
+        :key="tab.id"
+        :title="tab.title">
+        Not Implemented
+      </b-tab>
+    </b-tabs>
+  </b-container>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      tabs: [{
+        id: 1,
+        title: 'Esport',
+      }, {
+        id: 2,
+        title: 'Sport'
+      }, {
+        id: 3,
+        title: 'Casino'
+      }]
+    }
+  }
+}
+</script>
