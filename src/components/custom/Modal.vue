6a9d33e762844a09c44444fbf6d1b3c8aa274627
Mahendra <mtyagi@gammastack.com>
2018-10-29 12:02:34 +0200

    Add modal wrapper component with default settings

---
+++ src/components/custom/Modal.vue
@@ -0,0 +1,24 @@
+<script>
+  import bModal from 'bootstrap-vue/es/components/modal/modal'
+
+  export default {
+    extends: bModal,
+    props: {
+      'hide-footer': {
+        default: true
+      },
+      lazy: {
+        default: true
+      },
+      'header-bg-variant': {
+        default: 'primary'
+      },
+      'header-border-variant': {
+        default: 'primary'
+      },
+      'body-bg-variant': {
+        default: 'primary'
+      }
+    }
+  }
+</script>
