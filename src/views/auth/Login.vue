08258d7cc226a8425f82e143d052c5d1166d7072
Mahendra <mtyagi@gammastack.com>
2018-11-08 14:19:52 +0200

    Login form styles

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,41 +1,88 @@
 <template>
       <form @submit.prevent="submit">
-        <b-form-group
-          horizontal
-          :state="getState('login')"
-          :invalid-feedback="errors.username"
-          label="Username"
-          label-text-align="right"
-          label-class="form-label"
-          required="required">
-          <b-form-input
-            id="login-username"
-            v-model="fields.login"
-            :state="getState('login')"
-            required="required"/>
-        </b-form-group>
-        <b-form-group
-          horizontal
-          label="Password"
-          label-text-align="right"
-          label-class="form-label"
-          :state="getState('password')"
-          :invalid-feedback="errors.password">
-          <b-form-input
-            id="login-password"
-            v-model="fields.password"
-            :state="getState('password')"
-            type="password"
-            required="required"/>
-        </b-form-group>
-        <b-button :disabled="submitting"
-                  block
-                  type="submit">
-          Sign in
-        </b-button>
-        <b-link :to="'sign-up'">
-          Have no account yet? Sign up here
-        </b-link>
+        <b-row>
+          <b-col sm="3" align="right" class="mt-2">
+            <label for="login-username">
+              Username
+            </label>
+          </b-col>
+          <b-col sm="9">
+            <b-form-input
+                    id="login-username"
+                    :state="getState('login')"
+                    type="text"
+                    required/>
+          </b-col>
+        </b-row>
+        <b-row class="mt-4">
+          <b-col sm="3" align="right" class="mt-2">
+            <label for="login-username">
+              Password
+            </label>
+          </b-col>
+          <b-col sm="9">
+            <b-form-input
+                    id="login-username"
+                    :state="getState('password')"
+                    type="text"
+                    required/>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="6" class="mr-auto ml-auto">
+            <b-button class="my-4"
+                      :disabled="submitting"
+                      block
+                      type="submit">
+              Sign in
+            </b-button>
+          </b-col>
+        </b-row>
+        <b-row no-gutters>
+          <b-col align="center">
+            <b-link :to="'sign-up'">
+              Have no account yet? Sign up here
+            </b-link>
+          </b-col>
+        </b-row>
+
+
+
+
+
+
+        <!--<b-form-group-->
+          <!--horizontal-->
+          <!--:state="getState('login')"-->
+          <!--:invalid-feedback="errors.username"-->
+          <!--label="Username"-->
+          <!--label-text-align="right"-->
+          <!--label-class="form-label"-->
+          <!--:label-cols="4"-->
+          <!--breakpoint="lg"-->
+          <!--required="required">-->
+          <!--<b-form-input-->
+            <!--id="login-username"-->
+            <!--v-model="fields.login"-->
+            <!--:state="getState('login')"-->
+            <!--required="required"/>-->
+        <!--</b-form-group>-->
+        <!--<b-form-group-->
+          <!--horizontal-->
+          <!--label="Password"-->
+          <!--label-text-align="right"-->
+          <!--label-class="form-label"-->
+          <!--:label-cols="4"-->
+          <!--breakpoint="lg"-->
+          <!--:state="getState('password')"-->
+          <!--:invalid-feedback="errors.password">-->
+          <!--<b-form-input-->
+            <!--id="login-password"-->
+            <!--v-model="fields.password"-->
+            <!--:state="getState('password')"-->
+            <!--type="password"-->
+            <!--required="required"/>-->
+        <!--</b-form-group>-->
       </form>
 </template>
 
