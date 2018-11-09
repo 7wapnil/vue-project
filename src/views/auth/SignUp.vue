229f4df57a864965ffc24bf40ec03208cca4ed74
Mahendra <mtyagi@gammastack.com>
2018-11-09 13:02:51 +0200

    Resolve conflicts

--- src/views/auth/SignUp.vue
+++ src/views/auth/SignUp.vue
@@ -1,204 +1,274 @@
 <template>
-  <simple-layout>
-    <div class="row">
-      <div class="col">
-        <form @submit.prevent="submit">
-          <h2>Sign Up</h2>
-          <hr>
-          <div
-            v-show="isCurrentStep(1)"
-            class="step">
-            <b-form-group
-              :state="getState('username')"
-              :invalid-feedback="errors.username"
-              label="Username"
-              required="required">
-              <b-form-input
-                id="username"
-                v-model="fields.username"
-                :state="getState('username')"
-                required="required"/>
-            </b-form-group>
-            <b-form-group
-              :state="getState('email')"
-              :invalid-feedback="errors.email"
-              label="Email">
-              <b-form-input
-                id="email"
-                v-model="fields.email"
-                :state="getState('email')"
-                required="required"/>
-            </b-form-group>
-            <b-form-group
-              :state="getState('date_of_birth')"
-              :invalid-feedback="errors.date_of_birth"
-              label="Birth Date">
-              <flat-pickr
-                id="date_of_birth"
-                v-model="fields.date_of_birth"
-                :config="datePickerConfig"
-                :class="{'is-invalid': getState('date_of_birth') === false}"
-                class="form-control"/>
-            </b-form-group>
-            <b-form-group
-              :state="getState('password')"
-              :invalid-feedback="errors.password"
-              label="Password">
-              <b-form-input
-                id="password"
-                v-model="fields.password"
-                :state="getState('password')"
-                type="password"
-                required="required"/>
-            </b-form-group>
-            <b-form-group
-              :state="getState('password_confirmation')"
-              :invalid-feedback="errors.password_confirmation"
-              label="Password confirmation">
-              <b-form-input
-                id="password_confirmation"
-                v-model="fields.password_confirmation"
-                :state="getState('password_confirmation')"
-                type="password"
-                required="required"/>
-            </b-form-group>
+  <form @submit.prevent="submit">
+    <div v-show="isCurrentStep(1)" class="step">
+      <b-row no-gutters>
+        <b-col
+                sm="3"
+                align="right"
+                class="mt-2 mr-3">
+          <label for="signup-username">
+            Username
+          </label>
+        </b-col>
+        <b-col cols="6">
+          <b-form-input
+                  id="signup-username"
+                  v-model="fields.username"
+                  :state="getState('username')"
+                  type="text"
+                  aria-describedby="inputFeedbackUsername"
+                  required/>
+          <b-form-invalid-feedback
+                  id="inputFeedbackUsername"
+                  :invalid-feedback="errors.username"/>
+        </b-col>
+      </b-row>
+      <b-row no-gutters class="mt-4">
+        <b-col
+                sm="3"
+                align="right"
+                class="mt-2 mr-3">
+          <label for="signup-email">
+            Email
+          </label>
+        </b-col>
+        <b-col cols="6">
+          <b-form-input
+                  id="signup-email"
+                  v-model="fields.email"
+                  :state="getState('email')"
+                  type="text"
+                  aria-describedby="inputFeedbackEmail"
+                  required/>
+          <b-form-invalid-feedback
+                  id="inputFeedbackEmail"
+                  :invalid-feedback="errors.email"/>
+        </b-col>
+      </b-row>
+      <b-row no-gutters class="mt-4">
+        <b-col
+                sm="3"
+                align="right"
+                class="mt-2 mr-3">
+          <label for="signup-date_of_birth">
+            Birth Date
+          </label>
+        </b-col>
+        <b-col cols="6">
+          <flat-pickr
+                  id="signup-date_of_birth"
+                  v-model="fields.date_of_birth"
+                  :config="datePickerConfig"
+                  :class="{'is-invalid': getState('date_of_birth') === false}"
+                  class="form-control"/>
+          <b-form-invalid-feedback
+                  id="inputFeedbackBirthday"
+                  :invalid-feedback="errors.date_of_birth"/>
+        </b-col>
+      </b-row>
+      <b-row no-gutters class="mt-4">
+        <b-col
+                sm="3"
+                align="right"
+                class="mt-2 mr-3">
+          <label for="signup-password">
+            Password
+          </label>
+        </b-col>
+        <b-col cols="6">
+          <b-form-input
+                  id="signup-password"
+                  v-model="fields.password"
+                  :state="getState('password')"
+                  type="password"
+                  aria-describedby="inputFeedbackPassword"
+                  required/>
+          <b-form-invalid-feedback
+                  id="inputFeedbackPassword"
+                  :invalid-feedback="errors.password"/>
+        </b-col>
+      </b-row>
+      <b-row no-gutters class="mt-4">
+        <b-col
+                sm="3"
+                align="right"
+                class="mt-2 mr-3">
+          <label for="signup-password-confirmation">
+            Password confirmation
+          </label>
+        </b-col>
+        <b-col cols="6">
+          <b-form-input
+                  id="signup-password-confirmation"
+                  v-model="fields.password_confirmation"
+                  :state="getState('password_confirmation')"
+                  type="password"
+                  aria-describedby="inputFeedbackPasswordConfirmation"
+                  required/>
+          <b-form-invalid-feedback
+                  id="inputFeedbackPasswordConfirmation"
+                  :invalid-feedback="errors.password_confirmation"/>
+        </b-col>
+      </b-row>
 
-            <b-form-group
-              :state="getState('country')"
-              :invalid-feedback="errors.country"
-              label="Country">
-              <b-form-select
-                id="country"
-                :options="countries"
-                v-model="fields.country"
-                :state="getState('country')"
-                required="required"/>
-            </b-form-group>
-            <span v-if="tooYoung">You should be adult to continue signing up.</span>
-            <button
-              :disabled="uncompletedStep || tooYoung"
-              class="btn btn-dark btn-block"
-              @click.prevent="nextStep">
-              Continue
-            </button>
-          </div>
-          <div
-            v-show="isCurrentStep(2)"
-            class="step">
-            <b-form-group
-              :state="getState('first_name')"
-              :invalid-feedback="errors.first_name"
-              label="First name">
-              <b-form-input
-                id="first_name"
-                v-model="fields.first_name"
-                :state="getState('first_name')"
-                required="required"/>
-            </b-form-group>
-            <b-form-group
-              :state="getState('last_name')"
-              :invalid-feedback="errors.last_name"
-              label="Last name">
-              <b-form-input
-                id="last_name"
-                v-model="fields.last_name"
-                :state="getState('last_name')"
-                required="required"/>
-            </b-form-group>
+      <b-row no-gutters class="mt-4">
+        <b-col
+                sm="3"
+                align="right"
+                class="mt-2 mr-3">
+          <label for="signup-country">
+            Country
+          </label>
+        </b-col>
+        <b-col cols="6">
+          <b-form-select
+                  id="signup-country"
+                  :options="countries"
+                  v-model="fields.country"
+                  aria-describedby="inputFeedbackPasswordConfirmation"
+                  :state="getState('country')"
+                  required/>
+          <b-form-invalid-feedback
+                  id="inputFeedbackCountry"
+                  :invalid-feedback="errors.country"/>
+        </b-col>
+      </b-row>
 
-            <b-form-group
-              :state="getState('gender')"
-              :invalid-feedback="errors.gender"
-              label="Gender">
-              <b-form-select
-                id="gender"
-                :options="genders"
-                v-model="fields.gender"
-                :state="getState('gender')"
-                required="required"/>
-            </b-form-group>
+      <b-row v-if="tooYoung">
+        <b-col align="center" class="my-4">
+          You should be adult to continue signing up
+        </b-col>
+      </b-row>
 
-            <b-form-group
-              :state="getState('phone')"
-              :invalid-feedback="errors.phone"
-              label="Phone number">
-              <b-form-input
-                v-mask="'+############'"
-                id="phone"
-                v-model="fields.phone"
-                :state="getState('phone')"
-                required="required"/>
-            </b-form-group>
-            <span v-show="!isValidPhone">Invalid phone number.</span>
-            <b-form-group
-              :state="getState('city')"
-              :invalid-feedback="errors.city"
-              label="City">
-              <b-form-input
-                id="city"
-                v-model="fields.city"
-                :state="getState('city')"
-                required="required"/>
-            </b-form-group>
-
-            <b-form-group
-              :state="getState('street_address')"
-              :invalid-feedback="errors.street_address"
-              label="Address">
-              <b-form-input
-                id="street_address"
-                v-model="fields.street_address"
-                :state="getState('street_address')"
-                required="required"/>
-            </b-form-group>
-
-            <b-form-group
-              :state="getState('state')"
-              :invalid-feedback="errors.state"
-              label="Province">
-              <b-form-input
-                id="state"
-                v-model="fields.state"
-                :state="getState('state')"/>
-            </b-form-group>
-
-            <b-form-group
-              :state="getState('zip_code')"
-              :invalid-feedback="errors.zip_code"
-              label="ZIP Code">
-              <b-form-input
-                id="zip_code"
-                v-model="fields.zip_code"
-                :state="getState('zip_code')"/>
-            </b-form-group>
-            <b-form-group>
-              <b-form-checkbox
-                v-model="agree"
-                plain>
-                Confirm I am not underage, agree with T&C, agree with privacy policy
-              </b-form-checkbox>
-            </b-form-group>
-            <button
-              :disabled="submitting"
-              class="btn btn-dark btn-block"
-              @click.prevent="previousStep">
-              Back
-            </button>
-            <button
-              :disabled="submitting || ! agree"
-              type="submit"
-              class="btn btn-dark btn-block">
-              Register
-            </button>
-          </div>
-          <hr>
-          <router-link :to="'sign-in'">
-            Have account already? Sign in here
-          </router-link>
-        </form>
-      </div>
+      <b-row no-gutters>
+        <b-col
+                cols="6"
+                class="mr-auto ml-auto">
+      <b-button
+        :disabled="uncompletedStep || tooYoung"
+        class="ml-3"
+        block
+        @click.prevent="nextStep">
+        Continue
+      </b-button>
+        </b-col>
+      </b-row>
     </div>
-  </simple-layout>
+    <div v-show="isCurrentStep(2)" class="step">
+      <b-form-group
+        :state="getState('first_name')"
+        :invalid-feedback="errors.first_name"
+        label="First name">
+        <b-form-input
+          id="first_name"
+          v-model="fields.first_name"
+          :state="getState('first_name')"
+          required="required"/>
+      </b-form-group>
+      <b-form-group
+        :state="getState('last_name')"
+        :invalid-feedback="errors.last_name"
+        label="Last name">
+        <b-form-input
+          id="last_name"
+          v-model="fields.last_name"
+          :state="getState('last_name')"
+          required="required"/>
+      </b-form-group>
+
+      <b-form-group
+        :state="getState('gender')"
+        :invalid-feedback="errors.gender"
+        label="Gender">
+        <b-form-select
+          id="gender"
+          :options="genders"
+          v-model="fields.gender"
+          :state="getState('gender')"
+          required="required"/>
+      </b-form-group>
+
+      <b-form-group
+        :state="getState('phone')"
+        :invalid-feedback="errors.phone"
+        label="Phone number">
+        <b-form-input
+          v-mask="'+############'"
+          id="phone"
+          v-model="fields.phone"
+          :state="getState('phone')"
+          required="required"/>
+      </b-form-group>
+      <span v-show="!isValidPhone">Invalid phone number.</span>
+      <b-form-group
+        :state="getState('city')"
+        :invalid-feedback="errors.city"
+        label="City">
+        <b-form-input
+          id="city"
+          v-model="fields.city"
+          :state="getState('city')"
+          required="required"/>
+      </b-form-group>
+
+      <b-form-group
+        :state="getState('street_address')"
+        :invalid-feedback="errors.street_address"
+        label="Address">
+        <b-form-input
+          id="street_address"
+          v-model="fields.street_address"
+          :state="getState('street_address')"
+          required="required"/>
+      </b-form-group>
+
+      <b-form-group
+        :state="getState('state')"
+        :invalid-feedback="errors.state"
+        label="Province">
+        <b-form-input
+          id="state"
+          v-model="fields.state"
+          :state="getState('state')"/>
+      </b-form-group>
+
+      <b-form-group
+        :state="getState('zip_code')"
+        :invalid-feedback="errors.zip_code"
+        label="ZIP Code">
+        <b-form-input
+          id="zip_code"
+          v-model="fields.zip_code"
+          :state="getState('zip_code')"/>
+      </b-form-group>
+      <b-form-group>
+        <b-form-checkbox
+          v-model="agree"
+          plain>
+          Confirm I am not underage, agree with T&C, agree with privacy policy
+        </b-form-checkbox>
+      </b-form-group>
+      <button
+        :disabled="submitting"
+        class="btn btn-dark btn-block"
+        @click.prevent="previousStep">
+        Back
+      </button>
+      <button
+        :disabled="submitting || ! agree"
+        type="submit"
+        class="btn btn-dark btn-block">
+        Register
+      </button>
+    </div>
+    <b-row no-gutters>
+      <b-col align="center" class="mt-4">
+    <b-link @click="$emit('tab-changed', 0)">
+      Have account already? Sign in here
+    </b-link>
+      </b-col>
+    </b-row>
+  </form>
 </template>
 
 <script>
