c2877c79b3f4bacb8112699f8a0c556739584f63
Mahendra <mtyagi@gammastack.com>
2018-11-28 15:53:41 +0200

    Move main logo to another folder

--- vue.config.js
+++ vue.config.js
@@ -32,4 +32,24 @@ module.exports = {
     }
   },
 
+
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options.transformToRequire: {
+          'img': 'src',
+          'image': 'xlink:href',
+          'b-img': 'src',
+          'b-img-lazy': ['src', 'blank-src'],
+          'b-card': 'img-src',
+          'b-card-img': 'img-src',
+          'b-carousel-slide': 'img-src',
+          'b-embed': 'src'
+          },
+        return options
+      })
+  }
 }
