b7e37bb0b36b8da29ac4fa076c62cd072faa361e
Mahendra <mtyagi@gammastack.com>
2018-11-28 15:56:28 +0200

    Add global icon component

---
+++ src/components/custom/Icon.vue
@@ -0,0 +1,25 @@
+<template>
+    <i :style="{ 'font-size': size,'color': color }"
+       :class="'arc ' + 'arc-' + `${ name }`"/>
+</template>
+
+<script>
+  import '@/assets/styles/variables.scss'
+
+  export default {
+    props: {
+      name: {
+        type: String,
+        default: ''
+      },
+      size: {
+        type: String,
+        default: '12px'
+      },
+      color: {
+        type: String,
+        default: '#FFFFFF'
+      }
+    }
+  }
+</script>
