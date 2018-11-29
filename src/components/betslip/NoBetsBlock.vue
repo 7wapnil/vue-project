6866524a3432b4436d219247ad445760ccdd15b8
Mahendra <mtyagi@gammastack.com>
2018-11-29 15:53:34 +0200

    Lint styles

--- src/components/betslip/NoBetsBlock.vue
+++ src/components/betslip/NoBetsBlock.vue
@@ -1,17 +1,19 @@
 <template>
   <span v-if="!getBets.length">
     <div class="m-3">
-     <small class="text-arc-clr-iron">
-      <strong>
-        Betslip
-      </strong>
-     </small>
+      <small class="text-arc-clr-iron">
+        <strong>
+          Betslip
+        </strong>
+      </small>
     </div>
     <b-card
       bg-variant="transparent"
       class="mx-2 mb-2 pb-4 betslip-empty-card"
       no-body>
-      <img class="p-4" src="~@/assets/images/betslip/arc-logo-black.svg"/>
+      <img
+        class="p-4"
+        src="~@/assets/images/betslip/arc-logo-black.svg">
       <p class="text-center text-arc-clr-iron px-2 mb-0">Your betslip is currently empty<br>
         Click on any odds to add them<br>
         to betslip.
