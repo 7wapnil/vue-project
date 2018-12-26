6aa72565d3d1e8d0c1f6e2d47035b9bde066e46c
Mahendra <mtyagi@gammastack.com>
2018-12-26 17:40:50 +0200

    Add introduction area component

---
+++ src/components/custom/IntroductionArea.vue
@@ -0,0 +1,12 @@
+<template>
+    <b-container class="d-flex align-items-center justify-content-center bg-arc-clr-soil-darker"
+    style="height: 202px">
+        <b-row>
+            <b-col>
+                <h3>
+                    Welcome to ArcaneBet
+                </h3>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
