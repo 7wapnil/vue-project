5034cce0bb54e3bb06f3ea769317410b38b58968
Dinesh <dinesh@gammastack.com>
2019-01-14 16:32:07 +0200

    WIP: added new components, changed validation logic

---
+++ src/components/custom/ResponseErrorPanel.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-row
+    v-if="responseText"
+    no-gutters
+    class="mt-4"
+    align="center">
+    <b-col
+      lg="10"
+      md="12"
+      class="mr-auto ml-auto ml-3"
+      block>
+      <div class="main-body-error"> {{ responseText }}</div>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'ResponseErrorPanel',
+  props: {
+    responseText: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.main-body-error {
+    background-color: #7d7d7d;
+    border-radius: 4px;
+    padding: 10px;
+    margin: 10px 0;
+}
+</style>
---
+++ src/components/inputs/RegularInput.vue
@@ -0,0 +1,78 @@
+<template>
+  <b-row
+    no-gutters
+    class="mt-4">
+    <b-col
+      md="12"
+      lg="10"
+      class="mr-auto ml-auto ml-3"
+      block>
+      <div class="input-group">
+        <b-form-input
+          id=""
+          :type="type"
+          v-model="content"
+          required
+          autocomplete="off"
+          @input="handleInput"/>
+        <span
+          class="highlight"/>
+        <span
+          v-if="bottomBar"
+          class="bar"/>
+        <label class="text-input">{{ label }}</label>
+      </div>
+      {{ feedback }}
+      <b-form-invalid-feedback
+        v-if="feedback"
+        :id="feedbackId">{{ feedback }}</b-form-invalid-feedback>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'RegularInput',
+  props: {
+    label: {
+      type: String,
+      default: 'Name'
+    },
+    bottomBar: {
+      type: Boolean,
+      default: true
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    feedback: {
+      type: String,
+      default: ''
+    },
+    feedbackId: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      content: this.value
+    }
+  },
+  methods: {
+    handleInput (e) {
+      this.$emit('input', this.content)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/styles/components/input";
+@include regularInput();
+</style>
