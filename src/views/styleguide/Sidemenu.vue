08cc0f43a9d85f8eaafe059b95af50445cf7bbd3
Mahendra <mtyagi@gammastack.com>
2019-01-15 14:36:18 +0200

    Add color change panel

--- src/views/styleguide/Sidemenu.vue
+++ src/views/styleguide/Sidemenu.vue
@@ -42,6 +42,7 @@ export default {
         { name: 'Typography', page: 'styleguide-typography' },
         { name: 'Buttons', page: 'styleguide-buttons' },
         { name: 'Tabs', page: 'styleguide-tabs' },
+        { name: 'Betslip', page: 'styleguide-betslip' },
       ]
     }
   }
--- src/views/styleguide/TopNav.vue
+++ src/views/styleguide/TopNav.vue
@@ -1,12 +1,63 @@
 <template>
   <b-navbar
-    sticky
+    fixed="top"
+    toggleable="lg"
+    class="px-0"
+    type="dark"
     style="height: 80px">
-    <b-navbar-brand/>
-    <b-navbar-nav>
-      <b-nav-item>
-        ArcaneBet Styleguide
-      </b-nav-item>
-    </b-navbar-nav>
+    <b-container
+      fluid
+      style="max-width: 1920px"
+      class="p-0">
+      <b-navbar-brand
+        :to="{ name: 'home' }"
+        class="mr-0"/>
+
+      <b-navbar-nav id="top-navigation">
+        <b-nav-item
+          v-for="(siteColor, index) in siteColors"
+          :key="index"
+          :class="'navbar-menu-item ' + `${siteColor.background}`"
+          style="height: 80px"
+          @click="changeSiteColor(siteColor.category)">
+          <h6 class="m-0 font-weight-bold">
+            {{ siteColor.label }}
+          </h6>
+        </b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav>
+        <b-nav-item class="mr-4">
+          ArcaneBet Styleguide
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-container>
   </b-navbar>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      siteColors: [
+        { label: 'Green',
+          background: 'bg-arc-clr-sport-glow',
+          category: 'sports'
+        },
+        { label: 'Blue',
+          background: 'bg-arc-clr-esport-glow',
+          category: 'esports'
+        },
+        { label: 'Orange',
+          background: 'bg-arc-clr-inplay-glow',
+          category: 'live'
+        },
+      ]
+    }
+  },
+  methods: {
+    changeSiteColor (color) {
+      this.$emit('changeColor', color)
+    }
+  }
+}
+</script>
