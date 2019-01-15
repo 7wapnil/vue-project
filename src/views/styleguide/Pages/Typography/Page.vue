e2b572f9d1a0ad4144e4a17219bc4a4cdb3acb13
Mahendra <mtyagi@gammastack.com>
2019-01-15 09:10:00 +0200

    Add typography page to styleguide

--- src/views/styleguide/Pages/Typography/Page.vue
+++ src/views/styleguide/Pages/Typography/Page.vue
@@ -1,5 +1,17 @@
 <template>
-  <h3 class="m-4">
-    Typography
-  </h3>
+  <div>
+    <h3 class="m-4">
+      > Typography
+    </h3>
+    <b-card class="pl-2">
+      <h1>Heading H1 - 64pt</h1>
+      <h2>Heading H2 - 48pt</h2>
+      <h3>Heading H3 - 30pt</h3>
+      <h4>Heading H4 - 21pt</h4>
+      <h5>Heading H5 - 18pt</h5>
+      <h6>Heading H6 - 14pt</h6>
+      <span>Regular text - 16pt</span> <br>
+      <small>Small text - 14pt</small>
+    </b-card>
+  </div>
 </template>
--- src/views/styleguide/TopNav.vue
+++ src/views/styleguide/TopNav.vue
@@ -1,5 +1,12 @@
 <template>
-  <b-navbar style="height: 80px">
+  <b-navbar
+    sticky
+    style="height: 80px">
     <b-navbar-brand/>
+    <b-navbar-nav>
+      <b-nav-item>
+        ArcaneBet Styleguide
+      </b-nav-item>
+    </b-navbar-nav>
   </b-navbar>
 </template>
