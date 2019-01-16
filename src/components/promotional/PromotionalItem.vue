84b39d30d5d52f032fbcbf61f818fe524b4fa995
Mahendra <mtyagi@gammastack.com>
2019-01-16 10:52:31 +0200

    Fix promotional area styles

--- src/components/promotional/PromotionalItem.vue
+++ src/components/promotional/PromotionalItem.vue
@@ -1,14 +1,14 @@
 <template>
   <div v-if="!isLoggedIn">
     <div class="px-2 py-4 text-center text-arc-clr-iron">
-      Sign up now and get bonuses!
+      <h6 class="m-0">Sign up now and get bonuses!</h6>
     </div>
     <b-jumbotron
       bg-variant="dark"
       class="p-3 mb-0 mx-2 promotional-area">
-      <p class="my-4 text-center">
-        Promotional area with interesting things
-      </p>
+      <h6 class="my-4 text-center">
+        Promotional area <br> with interesting things
+      </h6>
       <b-row>
         <b-col>
           <b-button
@@ -16,10 +16,15 @@
             variant="arc-secondary">
             <b-row no-gutters>
               <b-col class="text-left">
-                Discover more
+                <h6 class="m-0">
+                  <strong>
+                    Discover more
+                  </strong>
+                </h6>
               </b-col>
               <b-col class="d-inline-flex align-items-center justify-content-end">
-                <icon name="arrow-right"/>
+                <icon name="arrow-right"
+                      size="10px"/>
               </b-col>
             </b-row>
           </b-button>
