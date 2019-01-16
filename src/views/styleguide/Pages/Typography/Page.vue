f57c91dd0cf5f30d2da6e7eec16d43cd9034195c
Mahendra <mtyagi@gammastack.com>
2019-01-16 14:48:08 +0200

    Add lists, paragraph and link to styleguide

--- src/views/styleguide/Pages/Typography/Page.vue
+++ src/views/styleguide/Pages/Typography/Page.vue
@@ -3,15 +3,55 @@
     <h3 class="m-4">
       Typography
     </h3>
+
     <b-card class="pl-2">
-      <h1>Heading H1 - 64pt</h1>
-      <h2>Heading H2 - 48pt</h2>
-      <h3>Heading H3 - 30pt</h3>
-      <h4>Heading H4 - 21pt</h4>
-      <h5>Heading H5 - 18pt</h5>
-      <h6>Heading H6 - 14pt</h6>
-      <span>Regular text - 16pt</span> <br>
-      <small>Small text - 14pt</small>
+        <h5 class="card-title text-arc-clr-gold">Headings</h5>
+          <h1>Heading H1 - 64pt</h1>
+          <h2>Heading H2 - 48pt</h2>
+          <h3>Heading H3 - 30pt</h3>
+          <h4>Heading H4 - 21pt</h4>
+          <h5>Heading H5 - 18pt</h5>
+          <h6>Heading H6 - 14pt</h6>
+          <span>Regular text - 16pt</span> <br>
+          <small>Small text - 14pt</small>
+
+        <h5 class="card-title my-4 text-arc-clr-gold">
+            Lists
+        </h5>
+        <ul>
+            <li>Cras justo odio</li>
+            <li>Dapibus ac facilisis in</li>
+            <li>Morbi leo risus</li>
+            <li>Porta ac consectetur ac</li>
+            <li>Vestibulum at eros</li>
+        </ul>
+
+        <ol>
+            <li>Cras justo odio</li>
+            <li>Dapibus ac facilisis in</li>
+            <li>Morbi leo risus</li>
+            <li>Porta ac consectetur ac</li>
+            <li>Vestibulum at eros</li>
+        </ol>
+
+        <h5 class="card-title my-4 text-arc-clr-gold">
+            Links
+        </h5>
+        <a href="#">Link</a>
+
+        <h5 class="card-title my-4 text-arc-clr-gold">
+            Paragraphs
+        </h5>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Duis vel eros at augue pharetra luctus. Vivamus molestie viverra orci,
+            nec mattis mi iaculis et. Aenean fermentum est est, ac efficitur justo varius vitae.
+            Integer tortor metus, consectetur vel congue vitae, fringilla ac turpis.
+            Aenean eu eros quis velit dictum aliquam eu in odio. Quisque eu purus orci.
+            Duis ac laoreet erat, ac venenatis nisl. Pellentesque mattis metus sed laoreet rhoncus.
+            Nulla tincidunt dolor odio. Quisque lobortis ullamcorper pellentesque.
+            Proin semper dapibus lacus, at tempor leo tempor eu.
+        </p>
     </b-card>
   </div>
 </template>
