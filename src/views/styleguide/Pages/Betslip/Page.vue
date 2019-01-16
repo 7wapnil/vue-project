3b366b0118ebca751a4345f2921ad5c9668833c5
Mahendra <mtyagi@gammastack.com>
2019-01-16 10:54:40 +0200

    Add betslip component to styleguide

--- src/views/styleguide/Pages/Betslip/Page.vue
+++ src/views/styleguide/Pages/Betslip/Page.vue
@@ -1,5 +1,28 @@
 <template>
-  <h3 class="m-4">
-    > Betslip
-  </h3>
+    <b-card>
+      <h3>
+        Betslip
+      </h3>
+        <b-card no-body class="p-4">
+            <b-row>
+                <b-col style="max-width: 295px"
+                       class="py-4 bg-arc-clr-soil-black">
+                    <betslip/>
+                </b-col>
+            </b-row>
+        </b-card>
+    </b-card>
 </template>
+<script>
+    import Betslip from './component/Betslip'
+    import BetslipItem from './component/BetslipItem'
+    import NoBetsBlock from './component/NoBetsBlock'
+
+    export default {
+        components: {
+          BetslipItem,
+          Betslip,
+          NoBetsBlock
+        }
+    }
+</script>
