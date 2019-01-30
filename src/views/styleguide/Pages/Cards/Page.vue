e28ce2806d2b0812e2db9eda8858adff233c6eba
Mahendra <mtyagi@gammastack.com>
2019-01-30 13:35:10 +0200

    Refactor styleguide Cards section

--- src/views/styleguide/Pages/Cards/Page.vue
+++ src/views/styleguide/Pages/Cards/Page.vue
@@ -1,42 +1,6 @@
 <template>
   <b-card>
-    <h3>Event cards</h3>
-
-    <b-row
-      no-gutters
-      class="py-4 pl-4 pr-0">
-      <b-col class="text-left d-inline-flex align-items-center">
-        <icon
-          class="pl-2 pr-3"
-          name="event-list-demo"
-          size="24px"/>
-        <h4 class="m-0 font-weight-light">
-          Football
-        </h4>
-      </b-col>
-      <b-col class="text-right">
-        <small class="mr-2">
-          1 event starts in 20min
-        </small>
-
-        <b-button
-          variant="arc-secondary"
-          class="ml-4 d-inline-flex align-items-center">
-          Football
-          <icon
-            class="pl-4"
-            name="arrow-right"
-            size="12px"/>
-        </b-button>
-      </b-col>
-    </b-row>
-    <b-row no-gutters>
-      <b-col>
-        <h6 class="text-arc-clr-gold pl-4">
-          Europe Premier League
-        </h6>
-      </b-col>
-    </b-row>
+    <event-list-header/>
 
     <sport-card-live/>
 
@@ -46,10 +10,11 @@
       Esport card
     </h3>
 
-    <esport-card/>
+    <esport-card class="mb-2"/>
+
+    <esport-card-v-s/>
 
     <arc-code code-text="<esport-card/>"/>
-
   </b-card>
 </template>
 
@@ -57,12 +22,16 @@
 import SportCardLive from './components/SportCardLive'
 import ArcCode from '../../Code/ArcCode'
 import EsportCard from './components/EsportCard'
+import EsportCardVS from './components/EsportCardVS'
+import EventListHeader from './components/EventListHeader'
 
 export default {
   components: {
     SportCardLive,
     ArcCode,
-    EsportCard
+    EsportCard,
+    EsportCardVS,
+    EventListHeader
   }
 }
 </script>
