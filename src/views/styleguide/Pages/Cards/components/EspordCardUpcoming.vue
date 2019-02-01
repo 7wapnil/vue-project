4fc7efc00ec9b50aa4aab941e766d23dfd85eb7d
Mahendra <mtyagi@gammastack.com>
2019-02-01 14:57:02 +0200

    Create upcoming esport card markup

---
+++ src/views/styleguide/Pages/Cards/components/EspordCardUpcoming.vue
@@ -0,0 +1,276 @@
+<template>
+  <b-card
+    no-body
+    style="min-width: 779px"
+    class="mb-1 upcoming-card"
+    bg-variant="arc-clr-soil-dark">
+    <b-card-body class="h-100 p-0">
+      <b-row
+        no-gutters
+        role="tab"
+        style="height: 90px">
+        <b-col style="max-width: 209px; min-width: 166px">
+          <b-row
+            no-gutters
+            class="p-3">
+            <b-col class="d-inline-flex align-items-start justify-content-start text-truncate">
+              <span class="d-inline-flex font-weight-bold text-arc-clr-iron text-uppercase mr-2 event-card-date">
+                Today
+              </span>
+              <span class="d-inline-flex font-weight-bold text-arc-clr-iron text-uppercase event-card-date">
+                20:30
+              </span>
+            </b-col>
+            <b-col cols="auto">
+              <b-row no-gutters>
+                <b-col class="d-inline-flex mr-3">
+                  <icon
+                    name="upcoming-event-replay"
+                    size="16px"
+                    color="arc-clr-soil-light"/>
+                </b-col>
+                <b-col class="d-inline-flex">
+                  <icon
+                    name="upcoming-event-statistic"
+                    size="18px"
+                    color="arc-clr-soil-light"/>
+                </b-col>
+              </b-row>
+            </b-col>
+          </b-row>
+          <b-row
+            no-gutters
+            class="pl-3 pr-5 pb-2">
+            <small class="text-arc-clr-iron event-card-name">
+              ESL Australia & NZ Championship
+            </small>
+          </b-row>
+        </b-col>
+        <b-col
+          class="event-card-inside-border-left"
+          style="min-width: 487px">
+          <b-row no-gutters>
+            <b-col class="px-3 pt-3 pb-2">
+              <b-row no-gutters>
+                <b-col class="d-inline-flex justify-content-center pb-2">
+                  <small class="m-0 text-arc-clr-iron team-name font-weight-bold">
+                    Faze Clan
+                  </small>
+                </b-col>
+                <div class="w-100"/>
+                <b-col>
+                  <b-button
+                    variant="arc-odd">
+                    6.66
+                  </b-button>
+                </b-col>
+              </b-row>
+            </b-col>
+            <b-col class="d-flex align-items-center justify-content-center p-1">
+              <b-row no-gutters>
+                <b-col class="pr-3">
+                  <icon
+                    name="sidemenu-game-icon"
+                    color="arc-clr-iron"
+                    size="24px"/>
+                </b-col>
+                <b-col class="px-3">
+                  <h5 class="mb-0 font-weight-light text-arc-clr-iron">
+                    VS
+                  </h5>
+                </b-col>
+                <b-col class="pl-3">
+                  <icon
+                    name="sidemenu-game-icon"
+                    color="arc-clr-iron"
+                    size="24px"/>
+                </b-col>
+              </b-row>
+            </b-col>
+
+            <b-col class="px-3 pt-3 pb-2">
+              <b-row no-gutters>
+                <b-col class="d-inline-flex justify-content-center pb-2">
+                  <small class="m-0 text-arc-clr-iron team-name font-weight-bold">
+                    Liverpool
+                  </small>
+                </b-col>
+                <div class="w-100"/>
+                <b-col>
+                  <b-button
+                    class="active"
+                    variant="arc-odd">
+                    7.77
+                  </b-button>
+                </b-col>
+              </b-row>
+            </b-col>
+          </b-row>
+        </b-col>
+        <b-col
+          v-b-toggle.esportcard-upcoming
+          cols="auto"
+          class="px-3 event-card-toggle-button">
+          <b-row
+            no-gutters
+            class="h-50 w-100">
+            <b-col class="d-flex align-items-center justify-content-center">
+              <icon
+                role="button"
+                color="arc-clr-soil-light"
+                tabindex="0"
+                size="7.5px"
+                name="upcoming-event-chevron-down"/>
+            </b-col>
+          </b-row>
+          <b-row
+            no-gutters
+            class="h-50 w-100">
+            <b-col class="d-flex align-items-start justify-content-center">
+              <h6 class="mb-0 mt-1 font-weight-bold">
+                + 3
+              </h6>
+            </b-col>
+          </b-row>
+        </b-col>
+        <b-link
+          class="col event-card-statistics-button event-card-inside-border-left"
+          style="min-width: 70px; max-width: 70px; min-height: 100%; position:relative">
+          <b-row
+            no-gutters
+            class="text-center h-100">
+            <b-row
+              no-gutters
+              class="h-50 w-100">
+              <b-col class="d-inline-flex justify-content-center align-items-center">
+                <icon
+                  color="arc-clr-soil-light"
+                  size="10px"
+                  name="upcoming-event-arrow-right"/>
+              </b-col>
+            </b-row>
+            <b-row
+              no-gutters
+              class="h-50 w-100">
+              <b-col class="d-inline-flex justify-content-center align-items-start">
+                <h6 class="mb-0 mt-1 font-weight-bold">
+                  + 124
+                </h6>
+              </b-col>
+            </b-row>
+          </b-row>
+        </b-link>
+      </b-row>
+    </b-card-body>
+    <b-row
+      no-gutters
+      role="tablist">
+      <b-col
+        align="center"
+        style="min-height: 0">
+        <b-collapse
+          id="esportcard-upcoming"
+          accordion="my-accordion">
+          <b-row
+            no-gutters
+            class="bg-arc-clr-soil-darker"
+            style="border-radius: 0 0 4px 4px">
+            <b-col class="py-2">
+              <b-row no-gutters>
+                <b-col style="max-width: 205px"/>
+                <b-col>
+                  <b-row
+                    class="event-card-inside-border-bottom"
+                    no-gutters>
+                    <b-col class="d-flex justify-content-end align-items-end pb-4 pr-4">
+                      <h6 class="m-0 text-arc-clr-iron">
+                        Over / Under
+                      </h6>
+                    </b-col>
+                    <b-col class="p-2">
+                      <b-col class="mb-1">
+                        <small class="text-arc-clr-iron">
+                          Under 2.15
+                        </small>
+                      </b-col>
+                      <b-button variant="arc-odd">
+                        7.77
+                      </b-button>
+                    </b-col>
+                    <b-col class="p-2">
+                      <b-col class="mb-1">
+                        <small class="text-arc-clr-iron">
+                          Under 2.15
+                        </small>
+                      </b-col>
+                      <b-button variant="arc-odd">
+                        7.77
+                      </b-button>
+                    </b-col>
+                  </b-row>
+                  <b-row
+                    class="event-card-inside-border-bottom"
+                    no-gutters>
+                    <b-col class="d-flex justify-content-end align-items-end pb-4 pr-4">
+                      <h6 class="m-0 text-arc-clr-iron">
+                        Over / Under
+                      </h6>
+                    </b-col>
+                    <b-col class="p-2">
+                      <b-col class="mb-1">
+                        <small class="text-arc-clr-iron">
+                          Under 2.15
+                        </small>
+                      </b-col>
+                      <b-button variant="arc-odd">
+                        7.77
+                      </b-button>
+                    </b-col>
+                    <b-col class="p-2">
+                      <b-col class="mb-1">
+                        <small class="text-arc-clr-iron">
+                          Under 2.15
+                        </small>
+                      </b-col>
+                      <b-button variant="arc-odd">
+                        7.77
+                      </b-button>
+                    </b-col>
+                  </b-row>
+                  <b-row no-gutters>
+                    <b-col class="d-flex justify-content-end align-items-end pb-4 pr-4">
+                      <h6 class="m-0 text-arc-clr-iron">
+                        Over / Under
+                      </h6>
+                    </b-col>
+                    <b-col class="p-2">
+                      <b-col class="mb-1 text-truncate">
+                        <small class="text-arc-clr-iron">
+                          Under 2.15
+                        </small>
+                      </b-col>
+                      <b-button variant="arc-odd">
+                        7.77
+                      </b-button>
+                    </b-col>
+                    <b-col class="p-2">
+                      <b-col class="mb-1">
+                        <small class="text-arc-clr-iron">
+                          Under 2.15
+                        </small>
+                      </b-col>
+                      <b-button variant="arc-odd">
+                        7.77
+                      </b-button>
+                    </b-col>
+                  </b-row>
+                </b-col>
+                <b-col style="max-width: 102px"/>
+              </b-row>
+            </b-col>
+          </b-row>
+        </b-collapse>
+      </b-col>
+    </b-row>
+  </b-card>
+</template>
