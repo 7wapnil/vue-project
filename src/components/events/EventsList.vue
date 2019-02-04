f59f7d0509ed9341a97261797bb88ac735e50576
Varun <vsharma@gammastack.com>
2019-02-04 12:52:58 +0200

    Fix amount of events returned on Events pages according to mock-ups

--- src/components/events/EventsList.vue
+++ src/components/events/EventsList.vue
@@ -92,10 +92,6 @@ export default {
       type: String,
       default: null
     },
-    live: {
-      type: Boolean,
-      default: true
-    },
     context: {
       type: String,
       default: null
@@ -104,7 +100,7 @@ export default {
   apollo: {
     events () {
       let subscription = null
-      let variables = { live: this.live }
+      let variables = {}
 
       if (this.tournamentId) {
         subscription = TOURNAMENT_EVENT_UPDATED
@@ -147,8 +143,6 @@ export default {
           titleKind: this.$route.params.titleKind,
           titleId: this.titleId,
           tournamentId: this.tournamentId,
-          inPlay: this.live,
-          upcoming: !this.live,
           categoryId: this.categoryId,
           context: this.context
         }
--- src/constants/graphql/event-context.js
+++ src/constants/graphql/event-context.js
@@ -1,3 +1,4 @@
 export const UPCOMING_FOR_TIME = 'upcoming_for_time'
 export const LIVE = 'live'
 export const UPCOMING_LIMITED = 'upcoming_limited'
+export const UPCOMING_UNLIMITED = 'upcoming_unlimited'
