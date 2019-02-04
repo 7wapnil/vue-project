03f75984771b36ee373e4c4afffe1d4b5774f42a
Mahendra <mtyagi@gammastack.com>
2019-02-04 15:31:14 +0200

    Add breadcrumbs markup

--- src/views/event/Page.vue
+++ src/views/event/Page.vue
@@ -1,84 +1,14 @@
 <template>
-  <div class="row">
-    <div class="col">
-      <b-card
-        v-if="event"
-        class="mt-4">
-        <div slot="header">{{ event.description }}</div>
-
-        <dl class="row">
-          <dt class="col-sm-3">Sport</dt>
-          <dd class="col-sm-9">{{ event.title.name }}</dd>
-          <dt class="col-sm-3">Time</dt>
-          <dd class="col-sm-9">{{ eventTime }}</dd>
-        </dl>
-
-        <dl
-          v-if="event.competitors"
-          class="row">
-          <dt class="col-sm-3">Competitors</dt>
-          <dd class="col-sm-9">
-            <p
-              v-for="competitor in event.competitors"
-              :key="competitor.id">
-              {{ competitor.name }}
-            </p>
-          </dd>
-        </dl>
-
-        <dl
-          v-for="(scope, index) in event.scopes"
-          :key="index"
-          class="row">
-          <dt class="col-sm-3 text-capitalize">{{ scope.kind }}</dt>
-          <dd class="col-sm-9">{{ scope.name }}</dd>
-        </dl>
-
-        <div v-if="event.state">
-          <dl
-            v-if="event.state.status_code"
-            class="row">
-            <dt class="col-sm-3">Status</dt>
-            <dd class="col-sm-9">{{ event.state.status }}</dd>
-          </dl>
-
-          <dl
-            v-if="event.state.score"
-            class="row">
-            <dt class="col-sm-3">Score</dt>
-            <dd class="col-sm-9">{{ event.state.score }}</dd>
-          </dl>
-
-          <dl
-            v-if="event.state.time"
-            class="row">
-            <dt class="col-sm-3">Time</dt>
-            <dd class="col-sm-9">{{ event.state.time }}</dd>
-          </dl>
-
-          <dl
-            v-if="event.state.period_scores.length > 0"
-            class="row">
-            <dt class="col-sm-3">Period Scores</dt>
-            <dd class="col-sm-9"/>
-          </dl>
-
-          <dl
-            v-for="period in event.state.period_scores"
-            :key="period.status_code"
-            class="row">
-            <dt class="col-sm-3">{{ period.status }}</dt>
-            <dd class="col-sm-9">{{ period.score }}</dd>
-          </dl>
-        </div>
-
-        <hr>
-
-        <markets-categories :event="event"/>
-
-      </b-card>
-    </div>
-  </div>
+  <b-row no-gutters>
+    <b-col style="height: 385px"
+           class="bg-arc-clr-soil-darker">
+      <b-row no-gutters>
+        <b-col>
+          <b-breadcrumb :items="items"/>
+        </b-col>
+      </b-row>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
@@ -96,6 +26,7 @@ export default {
     return {
       event: null,
       marketsLimit: UNLIMITED_QUERY,
+      items: ['Basketball', 'Europe', 'Eurocup', 'Charlotte Hornets VS New Orleans Pelicans']
     }
   },
   apollo: {
