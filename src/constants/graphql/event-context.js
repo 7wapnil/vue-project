b855d27cb3a56cb5b66d6192aa715cfc96cfe855
Varun <vsharma@gammastack.com>
2019-02-07 12:40:28 +0200

    Return back UPCOMING_FOR_TIME context for Event query

--- src/constants/graphql/event-context.js
+++ src/constants/graphql/event-context.js
@@ -1,4 +1,3 @@
-// export const UPCOMING_FOR_TIME = 'upcoming_for_time'
+export const UPCOMING_FOR_TIME = 'upcoming_for_time'
 export const LIVE = 'live'
 export const UPCOMING_LIMITED = 'upcoming_limited'
-export const UPCOMING_FOR_TIME = UPCOMING_LIMITED // temporary speed up the staging
