b41c4a6baaa96be403b319ffda93e75311dc68ad
Mahendra <mtyagi@gammastack.com>
2019-02-07 15:32:40 +0200

    Move event page header markup to separate component

--- src/views/event/Page.vue
+++ src/views/event/Page.vue
@@ -1,85 +1,8 @@
 <template>
   <div>
-    <b-row no-gutters>
-      <b-col>
-        <b-breadcrumb :items="items"/>
-      </b-col>
-    </b-row>
+    <b-breadcrumb :items="items"/>
 
-    <b-row
-      class="pt-4 pb-5"
-      no-gutters>
-      <b-col class="text-center pt-4">
-        <b-row no-gutters>
-          <b-col class="font-weight-bold my-4">
-            {{ event.competitors[0].name }}
-          </b-col>
-
-          <div class="w-100"/>
-
-          <b-col class="mt-1">
-            <icon
-              name="sidemenu-game-icon"
-              size="56px"/>
-          </b-col>
-        </b-row>
-      </b-col>
-
-      <b-col class="text-center">
-        <b-row no-gutters>
-          <b-col class="mb-2">
-            {{ event.description }}
-          </b-col>
-
-          <div class="w-100"/>
-
-          <b-col class="mt-4">
-            <span
-              style="font-size: 11px"
-              class="text-arc-clr-iron text-uppercase">
-              Starts
-            </span>
-          </b-col>
-
-          <div class="w-100"/>
-
-          <b-col class="mb-4">
-            {{ event.start_at | asFormattedDate }}
-          </b-col>
-
-          <div class="w-100"/>
-
-          <b-col class="mt-1">
-            <span
-              style="font-size: 11px"
-              class="text-arc-clr-iron text-uppercase">
-              Match type
-            </span>
-          </b-col>
-
-          <div class="w-100"/>
-
-          <b-col>
-            Best of 3
-          </b-col>
-        </b-row>
-      </b-col>
-
-      <b-col class="text-center pt-4">
-        <b-row no-gutters>
-          <b-col class="font-weight-bold my-4">
-            {{ event.competitors[1].name }}
-          </b-col>
-        </b-row>
-        <b-row no-gutters>
-          <b-col class="mt-1">
-            <icon
-              name="sidemenu-game-icon"
-              size="56px"/>
-          </b-col>
-        </b-row>
-      </b-col>
-    </b-row>
+    <header-section :event="event"/>
 
     <b-row
       class="mt-4"
@@ -103,11 +26,13 @@ import { EVENT_BY_ID_QUERY, EVENT_UPDATED } from '@/graphql'
 import { updateCacheList } from '@/helpers/graphql'
 import MarketsCategories from '@/components/markets/MarketsCategories'
 import EventTabs from '@/components/tabs/EventTabs'
+import HeaderSection from './HeaderSection'
 
 export default {
   components: {
     MarketsCategories,
-    EventTabs
+    EventTabs,
+    HeaderSection
   },
   data () {
     return {
