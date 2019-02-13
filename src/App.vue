3f4d50101079605da9e80776a787e545065fd792
Dinesh <dinesh@gammastack.com>
2019-02-13 11:42:19 +0200

    Remove SW code

--- src/App.vue
+++ src/App.vue
@@ -28,18 +28,6 @@ export default {
   },
   created () {
     this.fetchWallets()
-    console.log()
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(function (registration) {
-          // Registration was successful
-          console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function (err) {
-          // registration failed :(
-          console.log('ServiceWorker registration failed: ', err);
-        });
-      });
-    }
   },
   methods: {
     ...mapMutations('providers', {
