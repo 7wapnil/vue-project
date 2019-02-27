f8ded70bfb6bb1075c9d406a78a0f85b5bc408a0
Mahendra <mtyagi@gammastack.com>
2019-02-27 16:19:44 +0200

    Add circle component

---
+++ src/components/custom/ArcCircle.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :class="['bg-' + bgColor, 'text-' + color]"
+         class="d-flex align-items-center justify-content-center"
+         :style="{ 'width': size + 'px',
+                   'height': size + 'px',
+                   'border-radius': '50px'}">
+        <slot/>
+    </div>
+</template>
+<script>
+    export default {
+      props: {
+        size: {
+          type: Number,
+          default: 10
+        },
+        color: {
+          type: String,
+          default: 'arc-clr-white'
+        },
+        bgColor: {
+          type: String,
+          default: 'arc-clr-soil-dark'
+        }
+      }
+    }
+</script>
