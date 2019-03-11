250b0fa6ac851a0b9aa70f809ec16b3253cbd315
Dinesh <dinesh@gammastack.com>
2019-03-11 10:49:55 +0200

    Rollback code change.

--- src/views/account/AccountSidebar.vue
+++ src/views/account/AccountSidebar.vue
@@ -105,7 +105,7 @@ export default {
       return tabIndex
     }
   },
-  mounted () {
+  created () {
     if (this.$route.query.depositState) {
       this.changeTabIndex(this.depositTabIndex)
     }
