cbf56057b5bb43fb80e6f1ce9b6936ce88c9ed75
Dinesh <dinesh@gammastack.com>
2019-03-12 10:01:15 +0200

    Add formatting

--- src/graphql/bets.js
+++ src/graphql/bets.js
@@ -3,35 +3,35 @@ import gql from 'graphql-tag';
 export const BETS_LIST_QUERY = gql`
 query betsHistory ($kind: String = null, $page: Int, $per_page: Int) {
   bets (kind: $kind, page: $page, per_page: $per_page) {
-    collection{
-    id
-    created_at
-    amount
-    oddValue
-    status
-    market {
+    collection {
       id
-      name
-    }
-    event {
-      id
-      name
-    }
-    title {
-      kind
-    }
-    }
-    pagination{
-        count
-            items
-            page
-            pages
-            offset
-            last
-            next
-            prev
-            from
-            to
+      created_at
+      amount
+      oddValue
+      status
+        market {
+          id
+          name
+        }
+        event {
+          id
+          name
+        }
+        title {
+          kind
+        }
+      }
+    pagination {
+      count
+          items
+          page
+          pages
+          offset
+          last
+          next
+          prev
+          from
+          to
     }
   }
 }
