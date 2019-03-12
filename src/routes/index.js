c9725d44bacb2e3b343f5764aa861979e621761f
Mahendra <mtyagi@gammastack.com>
2019-03-12 09:34:16 +0200

    Scroll to top after page is change

--- src/routes/index.js
+++ src/routes/index.js
@@ -29,7 +29,10 @@ const router = new Router({
       name: 'NotFound',
       component: NotFound
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
 
 export default router
