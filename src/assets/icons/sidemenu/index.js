d3239382b2d5e1d8895506e73bf9aa4ecf27aa9f
Mahendra <mtyagi@gammastack.com>
2019-03-13 15:41:25 +0200

    Add esports basic icons

--- src/assets/icons/sidemenu/index.js
+++ src/assets/icons/sidemenu/index.js
@@ -1,6 +1,7 @@
 export const DEFAULT_ICON = 'sidemenu-game-icon';
 
 export default {
+  // Sports
   'badminton': 'sidemenu-badminton',
   'bandy': 'sidemenu-bandy',
   'baseball': 'sidemenu-baseball',
@@ -13,5 +14,14 @@ export default {
   'rugby': 'sidemenu-rugby',
   'soccer': 'sidemenu-soccer',
   'tennis': 'sidemenu-tennis',
-  'waterpolo': 'sidemenu-waterpolo'
+  'waterpolo': 'sidemenu-waterpolo',
+
+  // Esports
+  'call-of-duty': 'esports-cod',
+  'counter-strike': 'esports-csgo',
+  'dota-2': 'esports-dota2',
+  'league-of-legends': 'esports-league',
+  'overwatch': 'esports-overwatch',
+  'starcraft': 'esports-sc2',
+  'fifa': 'esports-fifa'
 }
