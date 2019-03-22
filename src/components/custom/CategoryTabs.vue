d7f4215014d2b7cef4af59e9666e39c2bdaa5dcb
Mahendra <mtyagi@gammastack.com>
2019-03-22 13:32:25 +0200

    Add navigation arrows to category tabs

--- src/components/custom/CategoryTabs.vue
+++ src/components/custom/CategoryTabs.vue
@@ -1,50 +1,78 @@
 <template>
-  <b-tabs
-    :lazy="lazy"
-    :value="value"
-    card
-    content-class="p-0 m-0"
-    class="category-tabs"
-    nav-class="bg-transparent px-4 py-0"
-    @input="emitInput">
-    <b-tab
-      v-for="(tab, index) in tabs"
-      :key="index"
-      no-body
-      title-link-class="category-tab px-4 py-3 bg-transparent">
-      <template slot="title">
-        <b-row no-gutters>
-          <b-col class="d-flex justify-content-center align-items-center">
-            <icon
-              :name="tab.icon"
-              color="arc-clr-iron"
-              size="18px"/>
-          </b-col>
-        </b-row>
-        <b-row no-gutters>
-          <b-col>
-            <span class="font-size-12 text-arc-clr-iron-light line-height-14 letter-spacing-3">
-              {{ tab.title }}
-            </span>
-          </b-col>
-        </b-row>
+    <b-tabs
+            :lazy="lazy"
+            :value="value"
+            content-class="p-0 m-0"
+            class="category-tabs"
+            v-model="categoryTabIndex"
+            nav-class="px-5"
+            @input="emitInput">
+
+      <template slot="tabs">
+
+        <b-nav-item v-if="categoryTabIndex > 0"
+                    style="position: absolute; left: 0; top: 10px"
+                    class="pointer left-side-navigation"
+                    @click="categoryTabIndex--">
+          <icon name="chevron-left"/>
+        </b-nav-item>
+
       </template>
 
-      <slot :tab="tab"/>
+      <b-tab
+              v-for="(tab, index) in tabs"
+              :key="index"
+              no-body
+              @click="$emit('category-tab-changed', tab.title)"
+              title-link-class="category-tab px-4 py-3 ">
+        <template slot="title">
+          <b-row no-gutters>
+            <b-col>
+              <icon
+                      :name="tab.icon"
+                      color="arc-clr-iron"
+                      size="18px"/>
+            </b-col>
+          </b-row>
+          <b-row no-gutters>
+            <b-col>
+            <span class="font-size-12 text-arc-clr-iron-light line-height-14">
+              {{ tab.title }}
+            </span>
+            </b-col>
+          </b-row>
+        </template>
 
-    </b-tab>
+        <slot :tab="tab"/>
+      </b-tab>
 
-    <div
-      slot="empty"
-      class="text-center text-muted">
-      No tabs. Try to check your connection.
-    </div>
 
-  </b-tabs>
+
+      <template slot="tabs">
+        <b-nav-item style="position: absolute; right: 0; top: 10px"
+                    v-if="categoryTabIndex !== tabs.length - 1"
+                    class="pointer right-side-navigation"
+                    @click="categoryTabIndex++">
+          <icon class="category-tab" name="chevron-right"/>
+        </b-nav-item>
+      </template>
+
+
+      <div
+              slot="empty"
+              class="text-center text-muted">
+        No tabs. Try to check your connection.
+      </div>
+    </b-tabs>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      categoryTabIndex: 0
+    }
+  },
   props: {
     tabs: {
       type: Array,
@@ -63,6 +91,6 @@ export default {
     emitInput (value) {
       this.$emit('input', value)
     }
-  }
+  },
 }
 </script>
