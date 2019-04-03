0ca0a618f17f17d63189a243837f8d00a562ac4a
Varun <vsharma@gammastack.com>
2019-04-03 12:23:48 +0300

    Update withdraw mutation handler

--- src/graphql/withdraws.js
+++ src/graphql/withdraws.js
@@ -1,18 +1,7 @@
 import gql from 'graphql-tag'
 
 export const WITHDRAW_MUTATION = gql`
-        mutation withdraw($amount: Float!, $walletId: ID!, $payment_method: String!, $payment_details: [PaymentMethodDetail]) {
-          withdraw(amount: $amount, walletId: $walletId, payment_method: $payment_method, payment_details: $payment_details ) {
-            amount
-            currency_id
-            customer_id
-            error_messages
-            id
-            initiator_id
-            initiator_type
-            kind
-            mode
-            status
-          }
-        }
-      `
+  mutation ($input: WithdrawInput!) {
+    withdraw(input: $input)
+  }
+`
