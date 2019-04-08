ec65d7a9da9f92b38247704480e07f62f3981dcd
Mahendra <mtyagi@gammastack.com>
2019-04-08 14:25:23 +0300

    Fix broken form labels

--- src/components/inputs/RegularInput.vue
+++ src/components/inputs/RegularInput.vue
@@ -1,27 +1,47 @@
 <template>
-  <div class="input-group-custom">
-    <b-form-input
-      :id="id"
-      :type="type"
-      v-model="content"
-      :disabled="disabled"
-      :name="id"
-      :autocomplete="autocomplete"
-      required
-      min="1"
-      @blur.native="handleBlur"
-      @keydown.enter.native="handleEnter"
-      @input="handleInput"/>
-  </div>
+  <b-row
+    no-gutters
+    class="mt-4">
+    <b-col
+      md="12"
+      lg="10"
+      class="mr-auto ml-auto ml-3"
+      block>
+      <div class="input-group-custom">
+        <b-form-input
+          :id="id"
+          :type="type"
+          v-model="content"
+          :disabled="disabled"
+          :name="id"
+          :autocomplete="autocomplete"
+          class="bg-arc-clr-white"
+          required
+          @blur.native="handleBlur"
+          @keydown.enter.native="handleEnter"
+          @input="handleInput"/>
+        <span
+          v-if="bottomBar"
+          class="bar"/>
+        <label class="text-input">{{ label }}</label>
+      </div>
+      {{ feedback }}
+    </b-col>
+  </b-row>
 </template>
 
 <script>
+
 export default {
   name: 'RegularInput',
   props: {
     label: {
       type: String,
       default: ''
+    },
+    bottomBar: {
+      type: Boolean,
+      default: true
     },
     type: {
       type: String,
