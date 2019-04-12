695e1eaa5276a125845873c2dc2879becfdb111d
Mahendra <mtyagi@gammastack.com>
2019-04-12 11:46:13 +0300

    Add logout modal

--- src/components/custom/ModalList.vue
+++ src/components/custom/ModalList.vue
@@ -1,16 +1,19 @@
 <template>
   <div v-if="isLoggedIn">
     <account-modal/>
+    <account-logout-modal/>
   </div>
 </template>
 
 <script>
 import AccountModal from '@/views/account/AccountModal'
+import AccountLogoutModal from '@/views/account/AccountLogoutModal'
 import { mapGetters } from 'vuex'
 
 export default {
   components: {
-    AccountModal
+    AccountModal,
+    AccountLogoutModal
   },
   computed: {
     ...mapGetters([
