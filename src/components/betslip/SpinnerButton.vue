bf8630f8f56ff9b0ca00a1cbc04b408a9259edf7
Mahendra <mtyagi@gammastack.com>
2019-04-17 11:31:36 +0300

    Get rid of custom spinner button

--- src/components/betslip/SpinnerButton.vue
+++ src/components/betslip/SpinnerButton.vue
@@ -1,22 +1,6 @@
 <template>
-  <b-row
-    class="btn-betslip-placing-bet"
-    no-gutters>
-    <b-col
-      class="d-flex align-items-center justify-content-center px-4"
-      cols="auto">
-      <arc-spinner/>
-    </b-col>
-    <b-col class="d-flex align-items-center pl-3">PLACING BET</b-col>
-  </b-row>
+  <b-button variant="betslip-placing-bet">
+    <b-spinner small variant="arc-clr-white" class="mr-2"/>
+      <slot/>
+  </b-button>
 </template>
-
-<script>
-import ArcSpinner from '@/components/custom/ArcSpinner'
-
-export default {
-  components: {
-    ArcSpinner
-  }
-}
-</script>
--- src/views/styleguide/Pages/Buttons/Page.vue
+++ src/views/styleguide/Pages/Buttons/Page.vue
@@ -1,5 +1,25 @@
 <template>
-  <h3 class="m-4">
-    Buttons
-  </h3>
+  <div>
+    <h3 class="m-4">Buttons</h3>
+     <b-row no-gutters>
+         <b-col class="p-4">
+             <spinner-button class="text-uppercase">
+                 Placing bet
+             </spinner-button>
+             <arc-code codeText="<spinner-button>">Button with spinner inside</arc-code>
+         </b-col>
+         <b-col/>
+     </b-row>
+  </div>
 </template>
+<script>
+    import SpinnerButton from '@/components/betslip/SpinnerButton'
+    import ArcCode from "@/views/styleguide/Code/ArcCode";
+
+    export default {
+      components: {
+        ArcCode,
+        SpinnerButton
+      }
+    }
+</script>
