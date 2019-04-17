f2f0503875b1506b925a18dfb11410a5515ba61b
Mahendra <mtyagi@gammastack.com>
2019-04-17 14:27:52 +0300

    Refactor spinner button

--- src/components/betslip/SpinnerButton.vue
+++ src/components/betslip/SpinnerButton.vue
@@ -1,6 +1,9 @@
 <template>
   <b-button variant="betslip-placing-bet">
-    <b-spinner small variant="arc-clr-white" class="mr-2"/>
-      <slot/>
+    <b-spinner
+      small
+      variant="arc-clr-white"
+      class="mr-2"/>
+    <slot/>
   </b-button>
 </template>
--- src/views/styleguide/Pages/Buttons/Page.vue
+++ src/views/styleguide/Pages/Buttons/Page.vue
@@ -1,25 +1,27 @@
 <template>
   <div>
-    <h3 class="m-4">Buttons</h3>
-     <b-row no-gutters>
-         <b-col class="p-4">
-             <spinner-button class="text-uppercase">
-                 Placing bet
-             </spinner-button>
-             <arc-code codeText="<spinner-button>">Button with spinner inside</arc-code>
-         </b-col>
-         <b-col/>
-     </b-row>
+    <h3 class="m-4">
+      Buttons
+    </h3>
+    <b-row no-gutters>
+      <b-col class="p-4">
+        <spinner-button class="text-uppercase">
+          Placing bet
+        </spinner-button>
+        <arc-code code-text="<spinner-button>">Button with spinner inside</arc-code>
+      </b-col>
+      <b-col/>
+    </b-row>
   </div>
 </template>
 <script>
-    import SpinnerButton from '@/components/betslip/SpinnerButton'
-    import ArcCode from "@/views/styleguide/Code/ArcCode";
+import SpinnerButton from '@/components/betslip/SpinnerButton'
+import ArcCode from '@/views/styleguide/Code/ArcCode';
 
-    export default {
-      components: {
-        ArcCode,
-        SpinnerButton
-      }
-    }
+export default {
+  components: {
+    ArcCode,
+    SpinnerButton
+  }
+}
 </script>
