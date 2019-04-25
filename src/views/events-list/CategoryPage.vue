e37b68962550f9d680686d529aa7afc72f5e3a7c
Jay <jhardia@gammastack.com>
2019-04-25 13:05:02 +0300

    Add events list component to category page

--- src/views/events-list/CategoryPage.vue
+++ src/views/events-list/CategoryPage.vue
@@ -7,36 +7,60 @@
         v-if="!showTitles"
         :title-id="$route.params.titleId"
         :category-id="$route.params.categoryId"
-        :tabs="tabsMapping" />
+        :tabs="tabsMapping"
+        @tab-changed="onFilterChange" />
+      <events-list
+        :title-id="$route.params.titleId"
+        :category-id="$route.params.categoryId"
+        :context="selectedFilter.context">
+
+        <template slot-scope="{ event }">
+          <hybrid-card
+            :event="event"
+            :tab-id="selectedFilter.id"/>
+        </template>
+      </events-list>
     </b-col>
   </b-row>
 </template>
 
 <script>
 import FilterTabs from '@/views/events-list/FilterTabs'
+import EventsList from '@/components/events/EventsList'
+import HybridCard from '@/views/events-list/HybridCard'
 import { UPCOMING_LIMITED } from '@/constants/graphql/event-context'
 import { LIVE, UPCOMING } from '@/constants/graphql/event-start-statuses'
 
 export default {
   components: {
-    FilterTabs
+    FilterTabs,
+    HybridCard,
+    EventsList
   },
   data () {
+    let liveTab = {
+      id: LIVE,
+      title: this.$i18n.t('homePage.live'),
+      context: LIVE
+    }
+    let upcomingTab = {
+      id: UPCOMING,
+      title: this.$i18n.t('homePage.upcoming'),
+      context: UPCOMING_LIMITED
+    }
     return {
-      tabsMapping: [{
-        id: LIVE,
-        title: this.$i18n.t('homePage.live'),
-        context: LIVE
-      }, {
-        id: UPCOMING,
-        title: this.$i18n.t('homePage.upcoming'),
-        context: UPCOMING_LIMITED
-      }]
+      tabsMapping: [liveTab, upcomingTab],
+      selectedFilter: upcomingTab
     }
   },
   computed: {
     showTitles () {
       return this.$route.name === 'title-kind'
+    }
+  },
+  methods: {
+    tabChanged(tab) {
+      this.selectedFilter = tab
     }
   }
 }
