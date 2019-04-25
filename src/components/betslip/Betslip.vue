24a3097fb2c0a7ce9b1b4655b69b61956048d4bf
Dinesh <dinesh@gammastack.com>
2019-04-25 10:41:25 +0300

    Clean up

--- src/components/betslip/Betslip.vue
+++ src/components/betslip/Betslip.vue
@@ -158,7 +158,6 @@ export default {
     ...mapGetters('betslip', [
       'betslipSubmittable',
       'getBets',
-      'getBetsCount',
       'getTotalReturn',
       'getTotalStakes',
       'acceptAllChecked',
