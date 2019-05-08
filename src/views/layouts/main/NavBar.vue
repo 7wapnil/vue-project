e4928807f4df0489af546b26912b9ee00c7c0167
Mahendra <mtyagi@gammastack.com>
2019-05-08 10:54:13 +0300

    Lint code

--- src/views/layouts/main/NavBar.vue
+++ src/views/layouts/main/NavBar.vue
@@ -10,40 +10,40 @@
         :to="{ name: 'home' }"
         class="mr-0"/>
       <b-navbar-nav id="top-navigation">
-          <b-nav-item
-            v-for="item in mainMenu"
-            :key="item.path"
-            :to="item.path"
-            class="navbar-menu-item font-weight-bold">
-            {{ item.label }}
-          </b-nav-item>
-        </b-navbar-nav>
-        <b-navbar-nav
-          v-if="!isLoggedIn"
-          style="max-width: 268px"
-          class="ml-auto">
-          <b-button
+        <b-nav-item
+          v-for="item in mainMenu"
+          :key="item.path"
+          :to="item.path"
+          class="navbar-menu-item font-weight-bold">
+          {{ item.label }}
+        </b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav
+        v-if="!isLoggedIn"
+        style="max-width: 268px"
+        class="ml-auto">
+        <b-button
 
-            variant="arc-secondary"
-            class="m-2"
-            @click="showAuthModal(0)">
-            Login
-          </b-button>
+          variant="arc-secondary"
+          class="m-2"
+          @click="showAuthModal(0)">
+          Login
+        </b-button>
 
-          <b-button
+        <b-button
 
-            variant="arc-primary"
-            class="m-2 mr-4"
-            @click="showAuthModal(1)">
-            Sign Up
-          </b-button>
-        </b-navbar-nav>
-        <b-navbar-nav
-          v-if="isLoggedIn"
-          class="ml-auto">
-          <balances-list/>
-          <user-profile-menu/>
-        </b-navbar-nav>
+          variant="arc-primary"
+          class="m-2 mr-4"
+          @click="showAuthModal(1)">
+          Sign Up
+        </b-button>
+      </b-navbar-nav>
+      <b-navbar-nav
+        v-if="isLoggedIn"
+        class="ml-auto">
+        <balances-list/>
+        <user-profile-menu/>
+      </b-navbar-nav>
 
     </b-container>
   </b-navbar>
