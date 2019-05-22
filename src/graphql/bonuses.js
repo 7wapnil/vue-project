1fcad172f6e40963507a15221ee9c1bd524ada0a
Mahendra <mtyagi@gammastack.com>
2019-05-22 11:26:21 +0300

    Camelize Bonus GraphQL types

--- src/graphql/bonuses.js
+++ src/graphql/bonuses.js
@@ -1,25 +1,25 @@
 import gql from 'graphql-tag'
 
 export const BONUS_CALCULATION_MUTATION = gql`
-        mutation deposit_bonus($amount: Float!, $code: String!) {
-          deposit_bonus(amount: $amount, code: $code) {
+        mutation depositBonus($amount: Float!, $code: String!) {
+          depositBonus(amount: $amount, code: $code) {
             bonus
-            real_money
+            realMoney
           }
         }
       `
 
 export const BONUSES_LIST_QUERY = gql`
-  query customer_bonuses {
-    customer_bonuses {
+  query customerBonuses {
+    customerBonuses {
       id
       code
-      expires_at
-      valid_for_days
-      rollover_balance
-      rollover_initial_value
-      max_rollover_per_bet
-      min_odds_per_bet
+      expiresAt
+      validForDays
+      rolloverBalance
+      rolloverInitialValue
+      maxRolloverPerBet
+      minOddsPerBet
       status
     }
   }
