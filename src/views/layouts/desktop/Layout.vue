c4c081c75e33d2cc88126b3f35d4c3c90e54eed6
Mahendra <mtyagi@gammastack.com>
2019-06-03 13:06:47 +0300

    Add mobile layout

---
+++ src/views/layouts/desktop/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <desktop-navigation-bar/>
+    <b-container
+      fluid
+      class="desktop-container">
+      <router-view :key="$route.fullPath"/>
+    </b-container>
+    <desktop-footer/>
+  </div>
+</template>
+
+<script>
+import DesktopNavigationBar from '@/views/layouts/desktop/NavigationBar'
+import DesktopFooter from '@/views/layouts/desktop/Footer'
+
+export default {
+  components: {
+    DesktopNavigationBar,
+    DesktopFooter
+  }
+}
+</script>
