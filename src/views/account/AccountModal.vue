6cec84af4bab0a94408416d9c592fcbfa5372c9c
Mahendra <mtyagi@gammastack.com>
2019-07-08 18:15:39 +0300

    Change profile modal layout / Add dynamic imports for layouts

--- src/views/account/AccountModal.vue
+++ src/views/account/AccountModal.vue
@@ -1,39 +1,37 @@
 <template>
   <modal
-    v-if="user"
     id="AccountModal"
     :lazy="false"
-    scrollable
+    :scrollable="!isMobile"
+    :dialog-class="[ isMobile ? 'bg-arc-clr-soil-darker h-100 m-0' : '']"
     size="lg"
     header-bg-variant="arc-clr-soil-darker"
     header-class="profile-modal-header"
     header-border-variant="arc-clr-soil-darker"
-    content-class="bg-arc-clr-soil-darker"
+    content-class="bg-arc-clr-soil-light h-100"
     body-class="p-0">
 
-    <modal-header :user="user"/>
+    <template #modal-header>
+      <modal-header/>
+    </template>
 
-    <vue-scroll>
-      <account-sidebar/>
-    </vue-scroll>
+    <component :is="accountLayout"/>
 
   </modal>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import AccountSidebar from './AccountSidebar'
 import ModalHeader from './AccountModalHeader'
 
 export default {
   components: {
-    AccountSidebar,
     ModalHeader
   },
   computed: {
-    ...mapGetters({
-      user: 'getUser'
-    })
+    accountLayout () {
+      const name = this.isMobile ? 'Mobile' : 'Desktop'
+      return () => import(`@/views/account/layouts/${name}Layout`)
+    }
   }
 }
 </script>
