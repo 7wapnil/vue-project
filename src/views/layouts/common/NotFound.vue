91aba45be0638616e3297fed5a1ca416ba4e4c0b
Mahendra <mtyagi@gammastack.com>
2019-07-24 16:32:24 +0300

    Lint code

--- src/views/layouts/common/NotFound.vue
+++ src/views/layouts/common/NotFound.vue
@@ -2,28 +2,37 @@
   <div class="bg-arc-clr-soil-darker min-vh-100">
     <b-row no-gutters>
       <b-col class="text-center mt-4">
-        <b-img :width="300" :src="require('@/assets/images/logo/arcanebet-logo.png')"/>
+        <b-img
+          :width="300"
+          :src="require('@/assets/images/logo/arcanebet-logo.png')"/>
       </b-col>
     </b-row>
     <b-row no-gutters>
-      <b-col cols="12" md="8" offset-md="2" class="pt-4 pt-md-5 pb-5 px-5">
-        <h1 v-if="!isMobile" class="p-4 text-arc-clr-iron font-weight-light">
-          {{ $t('noFoundPage.header')}}
+      <b-col
+        cols="12"
+        md="8"
+        offset-md="2"
+        class="pt-4 pt-md-5 pb-5 px-5">
+        <h1
+          v-if="!isMobile"
+          class="p-4 text-arc-clr-iron font-weight-light">
+          {{ $t('noFoundPage.header') }}
         </h1>
-        <span v-if="isMobile"
-              class="px-md-5 font-size-21 text-arc-clr-white font-weight-bold">
-          {{ $t('noFoundPage.header')}}
+        <span
+          v-if="isMobile"
+          class="px-md-5 font-size-21 text-arc-clr-white font-weight-bold">
+          {{ $t('noFoundPage.header') }}
         </span>
         <p class="font-size-18 letter-spacing-2 text-arc-clr-iron px-md-4 pb-4 pb-md-5 my-4">
-          {{ $t('noFoundPage.description')}}
+          {{ $t('noFoundPage.description') }}
         </p>
         <b-row
           no-gutters>
           <b-col class="text-left px-2 px-md-4">
             <b-button
               :to="{ path: '/esports' }"
-              size="lg"
               :block="isMobile"
+              size="lg"
               variant="arc-secondary"
               class="px-5 bg-arc-clr-esport-bg border-0 letter-spacing-2 font-weight-bold mb-4 text-uppercase">
               {{ $t('noFoundPage.esportButton') }}
