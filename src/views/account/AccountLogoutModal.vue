372ddad2905f76e63bc35bb0b74d517362b36e39
Mahendra <mtyagi@gammastack.com>
2019-07-25 13:39:54 +0300

    Add mobile styles for logout dialog

--- src/views/account/AccountLogoutModal.vue
+++ src/views/account/AccountLogoutModal.vue
@@ -8,6 +8,7 @@
     header-class="profile-modal-header"
     header-border-variant="arc-clr-soil-darker"
     body-bg-variant="arc-clr-soil-dark"
+    dialog-class="m-0 mx-md-auto my-md-5"
     body-class="p-4">
 
     <template slot="modal-header">
@@ -24,7 +25,7 @@
 
     <b-row no-gutter>
       <b-col class="text-center p-4">
-        <h5 class="mb-4 letter-spacing-2 font-weight-light">
+        <h5 class="mb-0 mb-md-4 letter-spacing-2 font-weight-light">
           Are you sure, you want to logout?
         </h5>
       </b-col>
@@ -32,16 +33,34 @@
     <b-row
       class="p-4"
       no-gutters>
-      <b-col class="text-center">
+      <b-col
+        cols="12"
+        md="6"
+        order="2"
+        order-md="1"
+        offset-md="0"
+        offset="3"
+        class="text-center">
         <b-button
-          class="secondary text-arc-clr-soil-black w-50"
+          :block="isMobile"
+          class="text-arc-clr-soil-black w-50"
+          variant="secondary"
           @click="hideModal">
           Cancel
         </b-button>
       </b-col>
-      <b-col class="text-center">
+      <b-col
+        cols="12"
+        md="6"
+        order="1"
+        order-md="2"
+        offset="3"
+        offset-md="0"
+        class="text-center mb-4 mb-md-0">
         <b-button
-          variant="user-profile-button w-50"
+          :block="isMobile"
+          class="w-50"
+          variant="user-profile-button"
           @click.prevent="logout">
           Logout
         </b-button>
