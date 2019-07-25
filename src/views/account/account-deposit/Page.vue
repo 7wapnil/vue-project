024723f0dd8ae2ae344e5dfc5e4f17d596654537
Mahendra <mtyagi@gammastack.com>
2019-07-25 09:51:26 +0300

    Add submitDeposit as callback to calculateBonus

--- src/views/account/account-deposit/Page.vue
+++ src/views/account/account-deposit/Page.vue
@@ -379,7 +379,7 @@ export default {
     }
   },
   methods: {
-    calculateBonus () {
+    calculateBonus (callback = null) {
       if (!this.isFormEmpty) {
         this.$apollo.mutate({
           mutation: BONUS_CALCULATION_MUTATION,
@@ -390,6 +390,9 @@ export default {
         }).then(({ data }) => {
           this.calculatedBonus = data.depositBonus.bonus
           this.bonusError = null
+          if (callback === this.submitDepositCallback) {
+            callback()
+          }
         }).catch(({ graphQLErrors }) => {
           this.calculatedBonus = null
           this.bonusError = graphQLErrors[0].message
@@ -397,6 +400,13 @@ export default {
       }
     },
     submitDeposit () {
+      if (this.isFormEmpty) {
+        this.submitDepositCallback()
+      } else {
+        this.calculateBonus(this.submitDepositCallback)
+      }
+    },
+    submitDepositCallback () {
       const input = {
         paymentMethod: this.paymentMethod.code,
         currencyCode: this.currency,
