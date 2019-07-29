cc5fc386ac2e1923f036088cb9f383a1edc2d21e
Mahendra <mtyagi@gammastack.com>
2019-07-29 14:46:47 +0300

    Add auth block styles

--- src/views/events-list/AuthBlock.vue
+++ src/views/events-list/AuthBlock.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mb-5">
+  <div>
     <b-row no-gutters>
       <b-col class="mb-3 text-center">
         <span class="font-size-14 letter-spacing-2 text-arc-clr-iron">
@@ -10,15 +10,15 @@
     <b-row no-gutters>
       <b-col class="text-right">
         <b-button
-          class="mr-4 w-75"
+          class="mx-2 w-75"
           variant="arc-secondary"
           @click="showAuthModal(0)">
           Login
         </b-button>
       </b-col>
-      <b-col>
+      <b-col class="text-left">
         <b-button
-          class="w-75"
+          class="mx-2 w-75"
           variant="arc-primary"
           @click="showAuthModal(1)">
           SignUp
