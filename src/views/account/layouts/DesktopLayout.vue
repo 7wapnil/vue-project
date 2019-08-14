598795a27a2f098b122886f0be17fdc5a37568bb
Mahendra <mtyagi@gammastack.com>
2019-08-14 14:28:05 +0300

    Add mobile content scrollbar

--- src/views/account/layouts/DesktopLayout.vue
+++ src/views/account/layouts/DesktopLayout.vue
@@ -1,6 +1,6 @@
 <template>
-<b-row no-gutters>
-    <b-col  class="profile-modal-sidebar">
+  <b-row no-gutters>
+    <b-col class="profile-modal-sidebar">
       <div class="profile-modal-sidebar-inner position-sticky">
         <profile-wallet @open-account-deposit-tab="changeTabIndex(depositTabIndex)"/>
         <b-nav
@@ -38,9 +38,9 @@
     </b-col>
 
     <b-col class="profile-modal-nav-content p-5">
-        <component :is="currentComponent"/>
+      <component :is="currentComponent"/>
     </b-col>
-</b-row>
+  </b-row>
 
 </template>
 <script>
