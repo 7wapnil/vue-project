6735d1f0100f7b668f8c806888806575f728c197
Mahendra <mtyagi@gammastack.com>
2019-08-28 16:01:08 +0300

    Add navigation to change password form

--- src/views/layouts/common/Layout.vue
+++ src/views/layouts/common/Layout.vue
@@ -47,6 +47,10 @@ export default {
     if (this.$route.query.resetPassword) {
       this.$bvModal.show('AuthModal')
     }
+
+    if (this.$route.query.changePassword) {
+      this.$bvModal.show('AccountModal')
+    }
   }
 }
 </script>
