8a99ce796723504d6f48202297064d84af1ce1ef
Varun <vsharma@gammastack.com>
2019-08-30 15:30:41 +0300

    [1628] Remove redirects from JS router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,13 @@ import Router from 'vue-router'
 import mainRoutes from './main'
 import StyleGuidePages from './styleguide_pages'
 import InformationPages from '@/routes/information_pages'
-import Redirects from '@/routes/redirects'
 import NotFound from '@/views/layouts/common/NotFound'
 import Maintenance from '@/views/layouts/common/Maintenance'
 import { setCookie } from '@/helpers/cookies'
 import moment from 'moment'
 import filters from '@/mixins/filters'
 
-const rootChildren = [...mainRoutes, ...InformationPages.routes, ...Redirects.routes]
+const rootChildren = [...mainRoutes, ...InformationPages.routes]
 
 const router = new Router({
   mode: 'history',
