34de3914c9bb9452388b22d79aef079604a91b20
Mahendra <mtyagi@gammastack.com>
2019-09-06 12:28:24 +0300

    Add new layout for email verification page

--- src/views/auth/EmailVerification.vue
+++ src/views/auth/EmailVerification.vue
@@ -1,38 +1,88 @@
 <template>
-  <simple-layout>
-    <loader v-if="sending"/>
-
-    <b-row v-if="!sending">
-      <b-col>
-        <b-alert
-          v-if="error"
-          variant="danger"
-          show>
-          <h4 class="alert-heading">Error accured!</h4>
-          <p>{{ error }}</p>
-          <router-link
-            :to="{ name: 'home' }"
-            class="btn btn-primary">Home page</router-link>
-        </b-alert>
-        <b-alert
-          v-if="!error"
-          variant="success"
-          show>
-          <h4 class="alert-heading">Well done!</h4>
-          <p>Your email successfully verified.</p>
-          <router-link
-            :to="{ name: 'home' }"
-            class="btn btn-primary">Home page</router-link>
-        </b-alert>
+  <div class="bg-arc-clr-soil-darker">
+    <b-row no-gutters>
+      <b-col class="text-center mt-4">
+        <b-img
+          :width="300"
+          :src="require('@/assets/images/logo/arcanebet-logo.png')"/>
       </b-col>
     </b-row>
-  </simple-layout>
+    <b-row no-gutters>
+      <b-col
+        cols="12"
+        md="8"
+        offset-md="2"
+        class="pt-4 pt-md-5 pb-5 px-5">
+
+        <loader v-if="sending"/>
+
+        <b-row
+          v-if="!sending"
+          no-gutters>
+          <b-col class="p-5 mb-5">
+            <div
+              v-if="error"
+              class="text-center">
+              <h4 class="font-weight-bold letter-spacing-2">
+                Error accured!
+              </h4>
+              <p class="letter-spacing-2">
+                {{ error }}
+              </p>
+            </div>
+            <div
+              v-if="!error"
+              class="text-center">
+              <h4 class="font-weight-bold letter-spacing-2">
+                Well done!
+              </h4>
+              <p class="letter-spacing-2">
+                Your account successfully activated
+              </p>
+            </div>
+          </b-col>
+        </b-row>
+
+        <b-row
+          no-gutters>
+          <b-col class="text-left px-2 px-md-4">
+            <b-button
+              :to="{ path: '/esports' }"
+              :block="isMobile"
+              size="lg"
+              variant="arc-secondary"
+              class="px-5 bg-arc-clr-esport-bg border-0 letter-spacing-2 font-weight-bold mb-4 text-uppercase">
+              {{ $t('noFoundPage.esportButton') }}
+            </b-button>
+          </b-col>
+          <b-col class="text-right px-2 px-md-4">
+            <b-button
+              :block="isMobile"
+              :to="{ path: '/sports'}"
+              size="lg"
+              variant="arc-secondary"
+              class="px-5 bg-arc-clr-sport-bg border-0 letter-spacing-2 font-weight-bold text-uppercase">
+              {{ $t('noFoundPage.sportButton') }}
+            </b-button>
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+    <arc-footer v-if="!isMobile"/>
+    <mobile-footer v-if="isMobile"/>
+  </div>
 </template>
 
 <script>
 import { VERIFY_EMAIL } from '@/graphql'
+import ArcFooter from '@/views/layouts/desktop/Footer'
+import MobileFooter from '@/views/layouts/mobile/footer/Footer'
 
 export default {
+  components: {
+    ArcFooter,
+    MobileFooter
+  },
   data () {
     return {
       sending: true,
