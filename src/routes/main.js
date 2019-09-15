67d92ba91e847190f689528bc1e508f24ebd5b32
Mahendra <mtyagi@gammastack.com>
2019-09-15 19:12:38 +0300

    Disable password reset link and form

--- src/routes/main.js
+++ src/routes/main.js
@@ -54,11 +54,6 @@ export default [
     ]
   },
   {
-    path: 'reset_password/:token',
-    name: 'reset_password',
-    component: () => import('@/views/auth/PasswordResetForm')
-  },
-  {
     path: 'email_verification/:token',
     name: 'email_verification',
     component: () => import('@/views/auth/EmailVerification')
