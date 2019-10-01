e885803eb4b1fe33fca977f98e321e9f073dc91b
Mahendra <mtyagi@gammastack.com>
2019-10-01 21:39:13 +0300

    Refactor site routing, add meta tags

--- src/routes/information_pages.js
+++ src/routes/information_pages.js
@@ -5,53 +5,49 @@ export const InformationPages = {
       name: 'support',
       title: 'support',
       icon: 'support',
-      meta: {
-        layout: {
-          mobile: 'information-page',
-          desktop: 'information-page'
-        }
-      },
+      meta: { sidemenu: 'info' },
+      component: () => import('@/views/layouts/common/Content'),
       children: [
         {
           path: 'contact-us',
           name: 'Contact Us',
           title: 'Contact Us',
-          meta: {
-            layout: {
-              mobile: 'information-page',
-              desktop: 'information-page'
-            }
-          },
+          meta: { sidemenu: 'info' },
           component: () => import('@/views/information-pages/support/contact-us/Page')
         },
         {
           path: 'terms-and-conditions',
           name: 'Terms And Conditions',
           title: 'Terms And Conditions',
+          meta: { sidemenu: 'info' },
           component: () => import('@/views/information-pages/support/terms-and-conditions/Page')
         },
         {
           path: 'betting-rules',
           name: 'Sports Betting Rules',
           title: 'Sports Betting Rules',
+          meta: { sidemenu: 'info' },
           component: () => import('@/views/information-pages/support/betting-rules/Page')
         },
         {
           path: 'privacy-policy',
           name: 'Privacy Policy',
           title: 'Privacy Policy',
+          meta: { sidemenu: 'info' },
           component: () => import('@/views/information-pages/support/privacy-policy/Page')
         },
         {
           path: 'cookie-policy',
           name: 'Cookie Policy',
           title: 'Cookie Policy',
+          meta: { sidemenu: 'info' },
           component: () => import('@/views/information-pages/support/cookie-policy/Page')
         },
         {
           path: 'faq',
           name: 'FAQ',
           title: 'FAQ',
+          meta: { sidemenu: 'info' },
           component: () => import('@/views/information-pages/support/faq/Page')
         }
       ]
@@ -61,37 +57,43 @@ export const InformationPages = {
       name: 'about',
       title: 'about',
       icon: 'arcanebet-default-icon',
-      component: () => import('@/views/layouts/information-page/Content'),
+      meta: { sidemenu: 'info' },
+      component: () => import('@/views/layouts/common/Content'),
       children: [
         {
           path: 'about-arcanebet',
           name: 'About arcanebet',
           title: 'About arcanebet',
+          meta: { sidemenu: 'info' },
           component: () => import('@/views/information-pages/about/main/Page')
-        }// , // FIXME: restore when affiliates system is back
-        // {
-        //   path: 'affiliates-arcanebet',
-        //   name: 'Affiliates',
-        //   title: 'Affiliates',
-        //   beforeEnter () { location.href = process.env.VUE_APP_AFFILIATE_URL }
-        // }
+        },
+        {
+          path: 'affiliates-arcanebet',
+          name: 'Affiliates',
+          title: 'Affiliates',
+          meta: { sidemenu: 'info' },
+          beforeEnter () { location.href = process.env.VUE_APP_AFFILIATE_URL }
+        }
       ]
     },
     {
       path: 'promotions',
       title: 'promotions',
-      component: () => import('@/views/layouts/information-page/Content'),
+      meta: { sidemenu: 'info' },
+      component: () => import('@/views/layouts/common/Content'),
       children: [
         {
           path: '',
           name: 'Promotions',
           title: 'Promotions',
+          meta: { sidemenu: 'info' },
           component: () => import('@/views/information-pages/promotions/main/Page')
         },
         {
           path: 'bonus-rules',
           name: 'Bonus Rules',
           title: 'Bonus Rules',
+          meta: { sidemenu: 'info' },
           component: () => import('@/views/information-pages/promotions/bonus-rules/Page')
         }
       ]
@@ -100,18 +102,21 @@ export const InformationPages = {
       path: 'responsible-gaming',
       name: 'Responsible Gaming',
       title: 'Responsible Gaming',
+      meta: { sidemenu: 'info' },
       icon: 'help',
-      component: () => import('@/views/layouts/information-page/Content'),
+      component: () => import('@/views/layouts/common/Content'),
       children: [
         {
           path: 'introduction',
           name: 'Introduction',
+          meta: { sidemenu: 'info' },
           title: 'Introduction',
           component: () => import('@/views/information-pages/responsible-gaming/introduction/Page')
         },
         {
           path: 'limits',
           name: 'Setting Limits',
+          meta: { sidemenu: 'info' },
           title: 'Setting Limits',
           component: () => import('@/views/information-pages/responsible-gaming/limits/Page')
         }
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,21 +1,8 @@
 import EventsList from '@/views/events-list/Page.vue'
 import CategoryPage from '@/views/events-list/CategoryPage.vue'
 import TournamentPage from '@/views/events-list/TournamentPage.vue'
-import Affiliates from '@/views/information-pages/affiliates/main/Page'
-import Promotions from '@/views/information-pages/promotions/main/Page'
 
 export default [
-  {
-    path: 'promotions',
-    component: () => import('@/views/layouts/information-page/Content'),
-    children: [
-      {
-        path: '',
-        name: 'Promotions',
-        component: Promotions
-      }
-    ]
-  },
   {
     path: ':titleKind',
     component: () => import('@/views/layouts/common/Content'),
@@ -67,16 +54,5 @@ export default [
     path: 'impersonate/:token',
     name: 'impersonation',
     component: () => import('@/views/auth/Impersonation')
-  },
-  {
-    path: 'affiliates',
-    component: () => import('@/views/layouts/information-page/Content'),
-    children: [
-      {
-        path: '',
-        name: 'affiliates',
-        component: Affiliates
-      }
-    ]
   }
 ]
