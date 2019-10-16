cb6e8bedc682a4e2d998efb91487f719bdb2749b
Mahendra <mtyagi@gammastack.com>
2019-10-16 11:44:24 +0300

    Add scoreboard progress bars styles and layout

--- src/views/event/header-section/competitors/SecondCompetitorSection.vue
+++ src/views/event/header-section/competitors/SecondCompetitorSection.vue
@@ -2,10 +2,25 @@
   <b-col>
     <b-row
       v-if="secondCompetitor"
-      class="h-100 d-flex align-items-center text-center"
+      class="h-100 text-center d-flex align-items-center justify-content-center"
       no-gutters>
-      <b-col v-if="!isMobile"
-             class="font-weight-bold my-4 mx-2 letter-spacing-2">
+      <b-col
+        class="h-100 d-flex align-items-center"
+        cols="auto">
+        <span class="event-description-scoreboard-progress-label">
+          {{ progressValue }} <br> %
+        </span>
+        <b-progress
+          :variant="progressVariant"
+          class="event-description-scoreboard-progress">
+          <b-progress-bar
+            :value="progressValue"
+            class="event-description-scoreboard-progress-bar"/>
+        </b-progress>
+      </b-col>
+      <b-col
+        v-if="!isMobile"
+        class="font-weight-bold m-4 letter-spacing-2 text-break">
         {{ secondCompetitor.name }}
       </b-col>
     </b-row>
@@ -18,6 +33,16 @@ export default {
     secondCompetitor: {
       type: Object,
       required: true
+    }
+  },
+  data () {
+    return {
+      progressValue: 55
+    }
+  },
+  computed: {
+    progressVariant () {
+      return this.$route.params.titleKind === 'esports' ? 'arc-clr-esport-bg' : 'arc-clr-sport-bg'
     }
   }
 }
