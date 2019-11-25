f8f2e8ad5a412fa1c1983fc3c4b52bd424f08102
Mahendra <mtyagi@gammastack.com>
2019-11-25 13:42:47 +0200

    Add casino components

---
+++ src/views/casino/CasinoContent.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-container
+    class="p-0"
+    fluid>
+    <b-row no-gutters>
+      <b-col>
+        <router-view name="gamelist">
+          <router-view/>
+        </router-view>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
---
+++ src/views/casino/CasinoSidebar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-row
+      v-if="!isMobile"
+      no-gutters>
+      <b-col class="p-3">
+        <casino-promotional-item/>
+      </b-col>
+    </b-row>
+    <b-row no-gutters>
+      <b-col>
+        <casino-sidemenu/>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import CasinoPromotionalItem from '@/views/casino/CasinoPromotionalItem'
+import CasinoSidemenu from '@/views/casino/CasinoSidemenu'
+
+export default {
+  components: {
+    CasinoPromotionalItem,
+    CasinoSidemenu
+  }
+}
+</script>
