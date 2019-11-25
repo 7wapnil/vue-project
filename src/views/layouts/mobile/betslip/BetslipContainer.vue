51e744d3cb586cb6d657aba8a3725254543a114c
Mahendra <mtyagi@gammastack.com>
2019-11-25 13:59:35 +0200

    Remove unused betslip components

--- src/views/layouts/mobile/betslip/BetslipContainer.vue
+++ src/views/layouts/mobile/betslip/BetslipContainer.vue
@@ -6,28 +6,28 @@
     @sidebar-opened="showContent = true"
     @sidebar-closed="showContent = false">
     <template #content>
-      <betslip v-if="showContent">
+      <router-view
+        v-if="showContent"
+        name="right">
         <template #close>
           <close-button
             :is-open="isBetslipOpen"
             position="left"
             @close:sidebar="closeBetslip"/>
         </template>
-      </betslip>
+      </router-view>
     </template>
   </sidebar>
 </template>
 
 <script>
 import Sidebar from '../sidemenu/Sidebar'
-import Betslip from '@/components/betslip/Betslip'
 import { mapGetters, mapMutations } from 'vuex'
 import CloseButton from '../sidemenu/CloseButton'
 
 export default {
   components: {
     Sidebar,
-    Betslip,
     CloseButton
   },
   data () {
