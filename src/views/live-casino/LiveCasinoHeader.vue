ce55d66a5dc781e4c2c880f3cc6300d46b4a6575
Mahendra <mtyagi@gammastack.com>
2019-11-25 13:41:02 +0200

    Add live casino components

---
+++ src/views/live-casino/LiveCasinoHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <b-row
+          no-gutters>
+    <b-col :class="[isMobile ? 'live-casino-header-mobile-container' :
+      'live-casino-header-desktop-container']">
+      <b-img
+              :src="require('@/assets/images/casino-games/arcane-live-casino-header-promo.png')"
+              :height="backgroundHeight"
+              fluid-grow
+              alt="Arcanebet-Casino-Promo"/>
+      <div class="live-casino-header-tab-container">
+        <slot/>
+      </div>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+const MOBILE_BACKGROUND_HEIGHT = '172'
+const DESKTOP_BACKGROUND_HEIGHT = '356'
+
+export default {
+  computed: {
+    backgroundHeight () {
+      return this.isMobile ? MOBILE_BACKGROUND_HEIGHT : DESKTOP_BACKGROUND_HEIGHT
+    }
+  }
+}
+</script>
+<style lang="scss"
+       scoped>
+  .live-casino-header {
+    &-tab-container {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+    }
+    &-mobile-container {
+      overflow: hidden;
+      height: 172px;
+    }
+    &-desktop-container {
+      overflow: hidden;
+      height: 291px;
+    }
+  }
+</style>
---
+++ src/views/live-casino/LiveCasinoPromotionalItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-row no-gutters>
+    <b-col class="promotional-block"/>
+  </b-row>
+</template>
+
+<style lang="scss"
+       scoped>
+    .promotional-block {
+        height: 259px;
+        border-radius: 4px;
+        box-shadow: 0 0 10px 2px #101010;
+        background-image:
+                url(#{'~@/assets/images/promotional/arcanebet-live-casino-page-small-banner.png'});
+    }
+</style>
