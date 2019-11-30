b168b8567a5b07cee15c912f892a8b5d3da31a7b
Varun <vsharma@gammastack.com>
2019-11-30 14:49:55 +0200

    [FIX][PROD] Fix terms and conditions page

--- src/views/information-pages/support/terms-and-conditions/Page.vue
+++ src/views/information-pages/support/terms-and-conditions/Page.vue
@@ -13,25 +13,7 @@ export default {
   components: { StaticContent },
   data () {
     return {
-      id: process.env.VUE_APP_CONTENTFUL_SUPPORT_TERMS_AND_CONDITIONS_ID,
-      tableOfContentItems: [
-        { name: 'General Terms', id: 'terms' },
-        { name: 'Definitions', id: 'definitions' },
-        { name: 'Your account', id: 'your-account' },
-        { name: 'Your responsibility', id: 'your-responsibility' },
-        { name: 'Your warranties & representations', id: 'warranties' },
-        { name: 'Financials', id: 'financials' },
-        { name: 'Withdrawals', id: 'withdrawals' },
-        { name: 'Wagering and making use of the player account', id: 'wagering' },
-        { name: 'Verification', id: 'verification' },
-        { name: 'Our rights', id: 'our-rights' },
-        { name: 'Unplayed, aborted or incomplete games', id: 'unplayed' },
-        { name: 'Investigation and suspension', id: 'investigation' },
-        { name: 'Acceptable use', id: 'acceptable' },
-        { name: 'Limitation of Liability', id: 'limitation' },
-        { name: 'Intellectual property', id: 'intellectual' },
-        { name: 'Governing law', id: 'law' }
-      ]
+      id: process.env.VUE_APP_CONTENTFUL_SUPPORT_TERMS_AND_CONDITIONS_ID
     }
   },
   metaInfo () {
