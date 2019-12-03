cb8d00031400a5fabbaab3a603daf6587dfb1c52
Mahendra <mtyagi@gammastack.com>
2019-12-03 18:58:12 +0200

    Move Overview item header indicators to separate component

---
+++ src/components/casino-games/play-items-list/OverviewItemHeaderIndication.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex flex-row">
+    <arc-circle
+      v-for="n in 5"
+      class="indication-item"
+      :class="{'active': n === 2}"
+      :size="6"/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .indication-item {
+        margin-left: 10px;
+    }
+    .active {
+        width: 20px !important;
+        background-color: #1b9cb0 !important;
+        transition: all .7s ease-in-out;
+    }
+</style>
