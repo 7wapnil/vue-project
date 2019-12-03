8ef99aa68e3671a747456167ae9f791c92492a88
Varun <vsharma@gammastack.com>
2019-12-03 16:06:58 +0200

    [IS-12] Automatically drop session for banned customers

--- src/libs/apollo/client.js
+++ src/libs/apollo/client.js
@@ -10,8 +10,10 @@ import arcanebetSession from '@/services/local-storage/session'
  * @todo Add error handler to logout user when unauthorized error received
  */
 
+const DROP_SESSION_IDENTIFIERS = ['AUTH_REQUIRED', 'BANNED']
+
 const errorLink = onError(({ graphQLErrors, networkError, operation, forward }) => {
-  if (graphQLErrors[0].message === 'AUTH_REQUIRED') {
+  if (DROP_SESSION_IDENTIFIERS.includes(graphQLErrors[0].message)) {
     arcanebetSession.dropSession()
     document.location.reload()
   }
