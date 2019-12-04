621be9aa0dc28bd3bf8c403b378de710d67fa304
Mahendra <mtyagi@gammastack.com>
2019-12-04 15:53:49 +0200

    Refactor infopage sidemenu routing and structure

--- src/views/layouts/information-page/InfoPageSidemenu.vue
+++ src/views/layouts/information-page/InfoPageSidemenu.vue
@@ -59,93 +59,93 @@ export default {
   data () {
     return {
       menuItems: [
-          {
-              id: 1,
-              title: 'Support',
-              icon: 'support',
-              route: { name: 'support' },
-              children: [
-              {
-                  id: 1.1,
-                  title: 'Contact us',
-                  route: { name: 'contact us' }
-              },
-              {
-                  id: 1.2,
-                  title: 'Terms and conditions',
-                  route: { name: 'terms and conditions' }
-              },
-              {
-                  id: 1.3,
-                  title: 'Sports betting rules',
-                  route: { name: 'sports betting rules' }
-              },
-              {
-                  id: 1.4,
-                  title: 'Privacy policy',
-                  route: { name: 'privacy policy' }
-              },
-              {
-                  id: 1.5,
-                  title: 'Cookie policy',
-                  route: { name: 'cookie policy' }
-              },
-              {
-                  id: 1.6,
-                  title: 'FAQ',
-                  route: { name: 'faq' }
-              }]
+        {
+          id: 1,
+          title: 'Support',
+          icon: 'support',
+          route: { name: 'support' },
+          children: [
+            {
+              id: 1.1,
+              title: 'Contact us',
+              route: { name: 'contact us' }
+            },
+            {
+              id: 1.2,
+              title: 'Terms and conditions',
+              route: { name: 'terms and conditions' }
+            },
+            {
+              id: 1.3,
+              title: 'Sports betting rules',
+              route: { name: 'sports betting rules' }
+            },
+            {
+              id: 1.4,
+              title: 'Privacy policy',
+              route: { name: 'privacy policy' }
+            },
+            {
+              id: 1.5,
+              title: 'Cookie policy',
+              route: { name: 'cookie policy' }
+            },
+            {
+              id: 1.6,
+              title: 'FAQ',
+              route: { name: 'faq' }
+            }]
+        },
+        {
+          id: 2,
+          title: 'About',
+          icon: 'arcanebet-default-icon',
+          route: { name: 'about' },
+          children: [{
+            id: 2.1,
+            title: 'About Arcanebet',
+            route: { name: 'about arcanebet' }
           },
           {
-              id: 2,
-              title: 'About',
-              icon: 'arcanebet-default-icon',
-              route: { name: 'about' },
-              children: [{
-                      id: 2.1,
-                      title: 'About Arcanebet',
-                      route: { name: 'about arcanebet' }
-              },
-                  {
-                      id: 2.2,
-                      title: 'Affiliates',
-                      route: { name: '' } //??
-                  }],
-          },
-          {
-              id: 3,
+            id: 2.2,
+            title: 'Affiliates',
+            route: { name: 'affiliates' }
+          }],
+        },
+        {
+          id: 3,
+          title: 'Promotions',
+          icon: 'promotional',
+          route: { name: 'promotions' },
+          children: [
+            {
+              id: 3.1,
               title: 'Promotions',
-              icon: 'promotional',
-              route: { name: 'promotions' },
-              children: [
-                  {
-                      id: 3.1,
-                      title: 'Promotions',
-                      route: { name: 'promotions' }
-                  },
-                  {
-                      id: 3.2,
-                      title: 'Bonus rules',
-                      route: { name: 'bonus rules' }
-              }],
-          },
-          {
-              id: 4,
-              title: 'Responsible gaming',
-              icon: 'help',
-              route: { name: 'responsible gaming' },
-              children: [
-                  {
-                      id: 4.1,
-                      title: 'Introduction',
-                      route: { name: 'introduction' }
-                  },
-                  {
-                      id: 4.2,
-                      title: 'Setting limits',
-                      route: { name: 'setting limits' }
-                  }],
-          },
+              route: { name: 'promotions' }
+            },
+            {
+              id: 3.2,
+              title: 'Bonus rules',
+              route: { name: 'bonus rules' }
+            }],
+        },
+        {
+          id: 4,
+          title: 'Responsible gaming',
+          icon: 'help',
+          route: { name: 'responsible gaming' },
+          children: [
+            {
+              id: 4.1,
+              title: 'Introduction',
+              route: { name: 'introduction' }
+            },
+            {
+              id: 4.2,
+              title: 'Setting limits',
+              route: { name: 'setting limits' }
+            }],
+        },
 
       ]
     }
