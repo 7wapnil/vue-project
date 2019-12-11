36d21665ce8271c6228db2c054d7314e7ea6507a
Mahendra <mtyagi@gammastack.com>
2019-12-11 09:48:22 +0200

    Lint

--- src/graphql/casino.js
+++ src/graphql/casino.js
@@ -1,5 +1,10 @@
 import gql from 'graphql-tag'
-import { CASINO_CATEGORIES_FIELDS, PLAY_ITEM_FIELDS, OVERVIEW_FIELDS, PAGINATION_FIELDS, CASINO_RECOMMENDED_GAMES_FIELDS, GAME_PROVIDERS_FIELDS } from './fields'
+import {
+  CASINO_CATEGORIES_FIELDS, PLAY_ITEM_FIELDS, OVERVIEW_FIELDS,
+  PAGINATION_FIELDS, CASINO_RECOMMENDED_GAMES_FIELDS,
+  EVERY_MATRIX_TRANSACTION_FIELDS, EVERY_MATRIX_SESSION_FIELDS,
+  GAME_PROVIDERS_FIELDS
+} from './fields'
 
 export const GAMES_QUERY = gql`
   query games($context: String = null, $page: Int, $perPage: Int) {
@@ -44,8 +49,8 @@ export const TABLES_CATEGORIES_QUERY = gql`
 `
 
 export const RECOMMENDED_GAMES_QUERY = gql`
-  query recommendedGames($original_game_id: Int!) {
-    recommendedGames(original_game_id: $original_game_id) {
+  query recommendedGames($originalGameId: String!) {
+    recommendedGames(originalGameId: $originalGameId) {
       ${CASINO_RECOMMENDED_GAMES_FIELDS}
     }
   }
@@ -76,6 +81,27 @@ export const GAME_PROVIDERS_QUERY = gql`
       pagination {
         ${PAGINATION_FIELDS}
       }
+    }
+  }
+`
+
+export const EVERY_MATRIX_TRANSACTIONS_LIST_QUERY = gql`
+  query everyMatrixTransactions($page: Int, $perPage: Int) {
+    everyMatrixTransactions(page: $page, perPage: $perPage) {
+      collection {
+        ${EVERY_MATRIX_TRANSACTION_FIELDS}
+      }
+      pagination {
+        ${PAGINATION_FIELDS}
+      }
+    }
+  }
+`
+
+export const CREATE_EVERY_MATRIX_SESSION_MUTATION = gql`
+  mutation ($walletId: Int, $playItemSlug: String!) {
+    createEveryMatrixSession(walletId: $walletId, playItemSlug: $playItemSlug) {
+      ${EVERY_MATRIX_SESSION_FIELDS}
     }
   }
 `
