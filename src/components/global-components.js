35316b8f99c33c68a37c8da231b046b26d5bd771
Varun <vsharma@gammastack.com>
2019-12-14 23:34:41 +0200

    [CD-79] Fix lazy loading for images on casino pages

--- src/components/global-components.js
+++ src/components/global-components.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import Router from 'vue-router'
+import MainLayout from '@/views/layouts/common/Layout'
 import SimpleLayout from '@/views/layouts/simple/Layout'
 import Loader from '@/components/custom/Loader'
 import Modal from '@/components/custom/Modal'
@@ -11,9 +12,16 @@ import ModalList from '@/components/custom/ModalList'
 import ArcCircle from '@/components/custom/ArcCircle'
 import PaymentMethodIcon from '@/components/PaymentMethodIcon'
 import ArcSeparator from '@/components/custom/ArcSeparator'
+import VueLazyload from 'vue-lazyload'
 
 Vue.use(Router)
 Vue.use(BootstrapVue)
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  attempt: 1
+})
+
+Vue.component('main-layout', MainLayout)
 Vue.component('simple-layout', SimpleLayout)
 Vue.component('loader', Loader)
 Vue.component('modal', Modal)
