245faf5abfc70c32af06ef2f7976212939ca7735
Mahendra <mtyagi@gammastack.com>
2019-12-17 15:14:21 +0200

    Add 'Extra Chilli' game promotion

--- src/views/casino/CasinoPromotionalItem.vue
+++ src/views/casino/CasinoPromotionalItem.vue
@@ -1,8 +1,24 @@
 <template>
   <b-row no-gutters>
-    <b-col class="promotional-block"/>
+    <b-col
+      class="promotional-block"
+      @click="pushGame"/>
   </b-row>
 </template>
+
+<script>
+export default {
+  methods: {
+    pushGame () {
+      this.$router.push({ name: 'casino-game',
+        params: { category: 'slots',
+          playItemSlug: 'extra-chilli-bigtimegaming'
+        }
+      })
+    }
+  }
+}
+</script>
 
 <style lang="scss"
        scoped>
@@ -10,6 +26,7 @@
         height: 259px;
         border-radius: 4px;
         box-shadow: 0 0 10px 2px #101010;
-        background-image: url(#{'~@/assets/images/promotional/arcanebet-casino-page-small-banner.png'});
+        background-image:
+                url(#{'~@/assets/images/promotional/arcanebet-casino-page-small-banner.png'});
     }
 </style>
