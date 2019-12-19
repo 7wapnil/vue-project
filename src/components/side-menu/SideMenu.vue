6b3c3dc5772321228c606433412c6bb65c4f4786
Mahendra <mtyagi@gammastack.com>
2019-12-19 13:41:21 +0200

    Change tab index if route was changed

--- src/components/side-menu/SideMenu.vue
+++ src/components/side-menu/SideMenu.vue
@@ -1,6 +1,7 @@
 <template>
   <b-nav
     tag="div"
+    class="flex-nowrap"
     vertical>
     <b-nav-item class="d-inline-flex align-items-center justify-content-start p-3 bg-arc-clr-soil-dark side-menu-categories w-100">
       <span class="text-arc-clr-iron letter-spacing-2 text-uppercase">
