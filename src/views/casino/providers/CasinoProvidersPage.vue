f0b99a98a2249e006bae390e518800da5c20f009
Mahendra <mtyagi@gammastack.com>
2019-12-22 16:01:34 +0200

    Add providers page

---
+++ src/views/casino/providers/CasinoProvidersPage.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <casino-providers-header/>
+    <div class="p-0 px-md-2 mb-md-2">
+      <b-row no-gutters>
+        <b-col
+          v-for="provider in providers"
+          :key="provider.id"
+          cols="12"
+          md="6">
+          <component
+            :is="providerComponent"
+            :provider="provider"/>
+        </b-col>
+      </b-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import CasinoProvidersHeader from '@/views/casino/providers/CasinoProvidersHeader'
+import { NETWORK_ONLY } from '@/constants/graphql/fetch-policy'
+import { GAME_PROVIDERS_QUERY } from '@/graphql'
+
+export default {
+  components: {
+    CasinoProvidersHeader
+  },
+  data () {
+    return {
+      providersCollection: [],
+      paginationProps: Object,
+      itemsPerPage: 20
+    }
+  },
+  apollo: {
+    gameProviders () {
+      return {
+        query: GAME_PROVIDERS_QUERY,
+        fetchPolicy: NETWORK_ONLY,
+        variables () {
+          return {
+            page: 1,
+            perPage: this.itemsPerPage
+          }
+        },
+        result ({ data }) {
+          this.providersCollection = data.gameProviders.collection
+          this.paginationProps = data.gameProviders.pagination
+        }
+      }
+    }
+  },
+  computed: {
+    providers () {
+      if (this.providersCollection) {
+        return this.providersCollection.filter(provider => provider.enabled === 'true')
+      }
+    },
+    providerComponent () {
+      const ProviderItem = () => import('@/views/casino/providers/CasinoProvidersItem')
+      const ProviderItemMobile = () => import('@/views/casino/providers/CasinoProvidersItemMobile')
+      return this.isMobile ? ProviderItemMobile : ProviderItem
+    }
+  }
+}
+</script>
