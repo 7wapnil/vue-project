5fff093b84944b54aa45d64ce8191cd6efc86673
Jay <jhardia@gammastack.com>
2020-01-08 18:17:44 +0200

    [BJ-117] hot streak and 1300px glitch fix

--- src/components/casino-games/play-items-list/promo-items/PromoItem.vue
+++ src/components/casino-games/play-items-list/promo-items/PromoItem.vue
@@ -4,7 +4,7 @@
     class="promo-item"
     @mouseover="hover = true"
     @mouseleave="hover = false"
-    @click="$router.push({ path: `/casino/${category.context}` })">
+    @click="clickHandler">
     <arc-circle
       v-if="isMobile && itemsPerPage <= 3"
       :size="30"
@@ -17,19 +17,13 @@
         name="chevron-left"/>
     </arc-circle>
     <div
-      v-if="isMobile || itemsPerPage > 2"
+      v-if="!isMobile || itemsPerPage > 2"
       class="promo-img" />
-    <div class="promo-text">
-      <h4 class="header">{{ isMobile ? this.$i18n.t('casino.promo.jackpot') : this.$i18n.t('casino.promo.totalJackpot') }}:</h4>
-      <h2 class="amount">€ {{ amount }}</h2>
-      <h4 class="play-these-games">{{ this.$i18n.t('casino.promo.playTheseGames') }}</h4>
-    </div>
+    <slot />
   </div>
 </template>
 
 <script>
-import { JACKPOT_TOTAL_QUERY } from '@/graphql'
-import { NETWORK_ONLY } from '@/constants/graphql/fetch-policy'
 export default {
   props: {
     category: {
@@ -39,12 +33,10 @@ export default {
     itemsPerPage: {
       type: Number,
       required: true,
-    }
-  },
-  data () {
-    return {
-      amount: 0,
-      increaseInterval: null,
+    },
+    clickHandler: {
+      type: Function,
+      default: () => {}
     }
   },
   computed: {
@@ -52,31 +44,6 @@ export default {
       let classNames = this.isMobile ? 'promo-item-mobile play-item-mobile' : 'promo-item-desktop play-item-desktop'
       if (this.isMobile && this.itemsPerPage > 3) classNames += ' tablet'
       return classNames
-    }
-  },
-  created () {
-    this.increaseInterval = setInterval(this.increaseAmount, 5000)
-  },
-  beforeDestroy () {
-    clearInterval(this.increaseInterval);
-  },
-  methods: {
-    increaseAmount () {
-      const min = 10
-      const max = 100
-      this.amount += Math.floor(Math.random() * (max - min + 1)) + min
-    }
-  },
-  apollo: {
-    jackpotTotal () {
-      return {
-        query: JACKPOT_TOTAL_QUERY,
-        fetchPolicy: NETWORK_ONLY,
-        pollInterval: 60 * 60 * 1000,
-        result ({ data }) {
-          this.amount = data.jackpotTotal.amount
-        }
-      }
     }
   },
 }
@@ -88,27 +55,32 @@ export default {
     background-repeat: no-repeat;
     background-size: cover;
     transition: all .4s ease-in-out;
+    flex: 0 0 auto;
     border-radius: 5px;
     position: relative;
-    flex: 0 0 auto;
-    display: flex;
 
     .promo-img {
       height: 100%;
       background-image: url(#{'~@/assets/images/casino-games/promo-coins.png'});
       background-repeat: no-repeat;
       background-size: cover;
+      width: 220px;
+      float: left;
     }
 
     .promo-text {
       width: 380px;
       padding: 35px 0;
       text-transform: uppercase;
-      .header {
+      float: right;
+      &-header {
         color: $arc-clr-gold;
         font-weight: $font-weight-bold;
       }
-      .play-these-games {color: $arc-clr-iron;}
+      .play-these-games {
+        color: $arc-clr-iron;
+        align-items: center;
+      }
     }
 
     &:hover {
@@ -120,17 +92,18 @@ export default {
       margin: 10px 10px 10px 0;
       cursor: pointer;
       height: 200px;
-      max-width: 600px;
+      max-width: 620px;
       width: 100%;
-      .promo-img {min-width: 220px;}
+      .play-these-games {display: flex;}
     }
     &-mobile {
-      margin: 10px 24px;
+      margin: 10px;
       text-align: center;
+      display: flex;
       .promo-text {
         width: 100%;
         padding: 12px 0;
-        .header {font-size: $h6-font-size;}
+        &-header {font-size: $h6-font-size;}
         .amount {font-size: $h3-font-size;}
         .play-these-games {font-size: $h6-font-size;}
       }
---
+++ src/components/casino-games/play-items-list/promo-items/PromoItemHotTables.vue
@@ -0,0 +1,46 @@
+<template>
+  <promo-item
+    :items-per-page="itemsPerPage"
+    :category="category"
+  >
+    <div class="promo-text">
+      <h4 class="promo-text-header">{{ this.$i18n.t('casino.promo.hotTables') }}</h4>
+      <h2 class="amount">{{ this.$i18n.t('casino.promo.hotStreak') }}</h2>
+      <h4 class="play-these-games">
+        {{ this.$i18n.t('casino.promo.onTheseTables') }}
+        <icon
+          v-if="!isMobile || itemsPerPage > 2"
+          color="arc-clr-soil-light"
+          size="20px"
+          name="upcoming-event-arrow-right"/>
+      </h4>
+    </div>
+  </promo-item>
+</template>
+
+<script>
+import PromoItem from '@/components/casino-games/play-items-list/promo-items/PromoItem.vue'
+
+export default {
+  components: {
+    PromoItem
+  },
+  props: {
+    category: {
+      type: Object,
+      required: true
+    },
+    itemsPerPage: {
+      type: Number,
+      required: true
+    }
+  },
+}
+</script>
+
+<style lang="scss"
+       scoped>
+  .play-these-games i {
+    margin-left: 20px;
+  }
+</style>
