bc2fe29c2aef3f34db4c5fa173aaca2632b6e930
Jay <jhardia@gammastack.com>
2020-02-06 13:43:13 +0200

    [RED-40] register btn fix

--- src/views/auth/SignupForm.vue
+++ src/views/auth/SignupForm.vue
@@ -159,7 +159,7 @@
     </b-row>
     <b-button
       id="signup-submit"
-      :disabled="submitting"
+      :disabled="isSubmitDisabled"
       variant="user-profile-button"
       type="submit"
       block>
@@ -204,6 +204,28 @@ export default {
   computed: {
     iconName () {
       return this.passwordFieldType === 'password' ? 'input-eye-solid' : 'input-eye-slash-solid'
+    },
+    isSubmitDisabled () {
+      const {
+        username,
+        email,
+        password,
+        country,
+        currency,
+        agreedWithPromotional,
+        agreedWithPrivacy
+      } = this.form.registration
+      const {
+        day,
+        month,
+        year
+      } = this.dobProxy
+
+      const hasProperFields = username && email &&
+        password && country && currency &&
+        agreedWithPromotional && agreedWithPrivacy &&
+        day && month && year
+      return !hasProperFields || this.submitting
     }
   },
   methods: {
